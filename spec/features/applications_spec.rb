00c211be0ef86e3f943bae5019a4811660acaa4b
Peter Karman <peter.karman@gsa.gov>
2016-05-18 10:03:54 -0500

    refactor tests to split up into smaller scenarios

--- spec/features/applications_spec.rb
+++ spec/features/applications_spec.rb
@@ -77,39 +77,53 @@ feature 'Applications CRUD' do
 end
 
 feature 'Admin User Approval' do
-  scenario 'new application' do
-    ActionMailer::Base.deliveries.clear
-
-    # create as normal user
+  scenario 'only admin user has option to approve application' do
     user = create(:user)
     login_as(user)
 
     visit new_users_application_path
 
     expect(page).to_not have_content('Approved')
+  end
 
-    fill_in 'Name', with: 'test application'
-    click_on 'Create'
-
-    expect(ActionMailer::Base.deliveries.count).to eq(2)
-    expect(page).to have_content('Success')
-
-    app = Application.all.last
-
-    # update as admin user
+  scenario 'admin user has option to approve application' do
+    app = create(:application)
     admin_user = create(:user, admin: true)
     login_as(admin_user)
 
     visit edit_users_application_path(app)
 
     expect(page).to have_content('Approved')
+  end
+end
 
+feature 'Email notification' do
+  scenario 'app creation generates email to owner and admin' do
+    user = create(:user)
+    login_as(user)
+
+    deliveries.clear # do not count User welcome
+
+    visit new_users_application_path
+    fill_in 'Name', with: 'test application'
+    click_on 'Create'
+
+    expect(deliveries.count).to eq(2)
+    expect(page).to have_content('Success')
+  end
+
+  scenario 'approval generates email to owner and admin' do
+    app = create(:application)
+    admin_user = create(:user, admin: true)
+    login_as(admin_user)
+
+    deliveries.clear # do not count User welcome
+
+    visit edit_users_application_path(app)
     choose('application_approved_true', option: 'true')
     click_on 'Update'
 
     expect(page).to have_content('Success')
-    app.reload
-    expect(app.approved?).to eq true
-    expect(ActionMailer::Base.deliveries.count).to eq(4)
+    expect(deliveries.count).to eq(2)
   end
-end 
+end
