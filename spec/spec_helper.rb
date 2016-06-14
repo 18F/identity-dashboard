f05dadb1c497e33441d8205a6e352b5970de9736
Moncef Belyamani <moncef.belyamani@gsa.gov>
2016-06-14 14:47:16 -0400

    Set up app to report test coverage to Code Climate
    **Why**: To make it easy to track our test coverage and code quality
    all in one place.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,8 @@
-if ENV.fetch('COVERAGE', false)
+if ENV['TRAVIS']
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+elsif ENV['COVERAGE']
   require 'simplecov'
-
-  if ENV['CIRCLE_ARTIFACTS']
-    dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
-    SimpleCov.coverage_dir(dir)
-  end
-
   SimpleCov.start 'rails'
 end
 
@@ -26,4 +23,4 @@ RSpec.configure do |config|
   config.order = :random
 end
 
-WebMock.disable_net_connect!(allow_localhost: true)
+WebMock.disable_net_connect!(allow: [/localhost/, /127\.0\.0\.1/, /codeclimate.com/])
