8fa51b71146cd727a8f1ce22f058a600093dc51e
Peter Karman <peter.karman@gsa.gov>
2016-06-27 14:29:58 -0500

    rubocop happiness

--- lib/saml_config.rb
+++ lib/saml_config.rb
@@ -10,32 +10,37 @@ module Saml
       @@settings
     end
 
+    # rubocop:disable AbcSize, MethodLength
     def build_settings
-      saml_env_config = Hashie::Mash.new(YAML.load_file("#{Rails.root}/config/saml.yml").fetch(Rails.env, {}))
+      config_file = "#{Rails.root}/config/saml.yml"
+      saml_env_config = Hashie::Mash.new(YAML.load_file(config_file).fetch(Rails.env, {}))
       if Rails.env.test? || Rails.env.development?
         saml_env_config[:sp_private_key] = File.read("#{Rails.root}/config/saml_private.key")
       end
-      Hashie::Mash.new(issuer:                        'https://dashboard.login.gov',
-                       idp_sso_target_url:            ENV.fetch('IDP_SSO_URL', saml_env_config[:sso_url]),
-                       idp_slo_target_url:            ENV.fetch('IDP_SLO_URL', saml_env_config[:slo_url]),
-                       idp_cert_fingerprint:          ENV.fetch('IDP_FINGERPRINT', saml_env_config[:idp_fingerprint]),
-                       name_identifier_format:        ENV.fetch('IDP_NAME_ID_FORMAT', saml_env_config[:name_id_format]),
-                       certificate:                   ENV.fetch('SP_CERTIFICATE', saml_env_config[:sp_certificate]),
-                       private_key:                   OpenSSL::PKey::RSA.new(
-                         ENV.fetch('SP_PRIVATE_KEY', saml_env_config[:sp_private_key]),
-                         ENV.fetch('SP_PRIVATE_KEY_PASSWORD', saml_env_config[:sp_private_key_password])
-                       ).to_s,
-                       allowed_clock_drift:           5.minutes,
-                       authn_context:                 'http://idmanagement.gov/ns/assurance/loa/1',
-                       single_signon_service_binding: 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
-                       double_quote_xml_attribute_values: true,
-                       security: {
-                         authn_requests_signed: true,
-                         embed_sign: true,
-                         digest_method: 'http://www.w3.org/2001/04/xmlenc#sha256',
-                         signature_method: 'http://www.w3.org/2001/04/xmldsig-more#rsa-sha256'
-                       })
+      Hashie::Mash.new(
+        issuer: 'https://dashboard.login.gov',
+        idp_sso_target_url: ENV.fetch('IDP_SSO_URL', saml_env_config[:sso_url]),
+        idp_slo_target_url: ENV.fetch('IDP_SLO_URL', saml_env_config[:slo_url]),
+        idp_cert_fingerprint: ENV.fetch('IDP_FINGERPRINT', saml_env_config[:idp_fingerprint]),
+        name_identifier_format: ENV.fetch('IDP_NAME_ID_FORMAT', saml_env_config[:name_id_format]),
+        certificate: ENV.fetch('SP_CERTIFICATE', saml_env_config[:sp_certificate]),
+        private_key: OpenSSL::PKey::RSA.new(
+          ENV.fetch('SP_PRIVATE_KEY', saml_env_config[:sp_private_key]),
+          ENV.fetch('SP_PRIVATE_KEY_PASSWORD', saml_env_config[:sp_private_key_password])
+        ).to_s,
+        allowed_clock_drift: 5.minutes,
+        authn_context: 'http://idmanagement.gov/ns/assurance/loa/1',
+        single_signon_service_binding: 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST',
+        double_quote_xml_attribute_values: true,
+        security: {
+          authn_requests_signed: true,
+          embed_sign: true,
+          digest_method: 'http://www.w3.org/2001/04/xmlenc#sha256',
+          signature_method: 'http://www.w3.org/2001/04/xmldsig-more#rsa-sha256'
+        }
+      )
     end
+    # rubocop:enable AbcSize, MethodLength
 
     def logout_url
       settings[:idp_slo_target_url] || settings[:idp_sso_target_url].gsub(/auth$/, 'logout')
