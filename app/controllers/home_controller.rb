ecfd289693d4577920b6897a978adb8ed3473a66
Zach Margolis <Zachary_B_Margolis@omb.eop.gov>
2016-11-02 17:02:31 -0400

    Add basic deploy dashboad to homepage
    **Why**:
    So we can see what's going on

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
+    @deploy_statuses = deploy_status_checker.check!
+  end
+
+  protected
+
+  def deploy_status_checker
+    @deploy_status_checker ||= DeployStatusChecker.new
   end
 end
