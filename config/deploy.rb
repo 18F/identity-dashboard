a737fe3967152de81cd07074dd70b5623a2e8ada
Zach Margolis <Zachary_B_Margolis@omb.eop.gov>
2016-11-04 14:34:03 -0400

    Add missing require
    **Why**:
    Because I got this error when I deployed:
    
    ```
    NoMethodError: undefined method `to_json' for #<Hash:0x007fd504082470>
    Did you mean?  to_s
    ```

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,6 +28,7 @@ namespace :deploy do
   desc 'Write deploy information to deploy.json'
   task :deploy_json do
     on roles(:web), in: :parallel do
+      require 'json'
       require 'stringio'
 
       within current_path do
