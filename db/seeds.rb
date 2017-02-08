5974b6545622cf93ae112dc05fcdd393607ceeab
Amos Stone <amos@amosarts.com>
2017-02-08 10:41:36 -0800

    Adds agency seeds (#63)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+%w(DHS GSA RRB).each do |str|
+  Agency.find_or_create_by(name: str)
+end
