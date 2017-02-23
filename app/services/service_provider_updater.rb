cc3630de6829b57d953c967efe9d8edfc3f4d792
Jessie A. Young <jessie.a.young@gmail.com>
2017-02-23 10:05:00 -0800

    Send dashboard API token in headers when hitting IDP API
    **Why**: See https://github.com/18F/identity-idp/pull/1108

--- app/services/service_provider_updater.rb
+++ app/services/service_provider_updater.rb
@@ -1,6 +1,6 @@
 class ServiceProviderUpdater
   def ping
-    resp = HTTParty.post(idp_url)
+    resp = HTTParty.post(idp_url, headers: token_header)
     resp.code == 200
   end
 
@@ -8,5 +8,9 @@ class ServiceProviderUpdater
 
   def idp_url
     ENV['IDP_SP_URL'] || 'https://login.gov/api/service_provider'
+  end
+
+  def token_header
+    { 'X-LOGIN-DASHBOARD-TOKEN' => ENV['LOGIN_DASHBOARD_TOKEN'] }
   end
 end
