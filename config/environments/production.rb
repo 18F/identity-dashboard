53322197c7f931b092e7d09c5b5390856f664a83
Jessie A. Young <jessie.a.young@gmail.com>
2017-02-28 08:30:37 -0800

    Figaro values are always string objects

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   config.eager_load = true
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
-  config.serve_static_files = Figaro.env.serve_static_files == true
+  config.serve_static_files = Figaro.env.serve_static_files == 'true'
   config.middleware.use Rack::Deflater
   config.assets.js_compressor = :uglifier
   config.assets.compile = false
