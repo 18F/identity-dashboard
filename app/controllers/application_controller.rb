f737088d3b3a0c81553dc7c337f2bbddcfbdef23
Jessie A. Young <jessie.a.young@gmail.com>
2017-03-03 09:36:53 -0800

    Remove `FORCE_USER` references
    **Why**: We do not need this because users can auth with Login.gov

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,18 +5,6 @@ class ApplicationController < ActionController::Base
 
   rescue_from Pundit::NotAuthorizedError, with: :render_401
 
-  def current_user
-    if Figaro.env.FORCE_USER
-      User.find(Figaro.env.FORCE_USER)
-    else
-      super
-    end
-  end
-
-  def user_signed_in?
-    Figaro.env.FORCE_USER.present? || super
-  end
-
   def new_session_path(_scope)
     new_user_session_path
   end
