651a3e56c61a394de4c67f88fc33614446e8f107
Jessie A. Young <jessie.a.young@gmail.com>
2017-03-21 11:02:46 -0700

    Add unit test for user method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,32 @@ describe User do
 
   let(:user) { build(:user) }
 
+  describe '.without_other_user_group' do
+    context 'user group id passed in' do
+      it 'returns users without a user group or with user group id passed in' do
+        _user_in_other_user_group = create(:user, user_group: create(:user_group))
+        user_without_group = create(:user)
+        users_in_group = create_list(:user, 2)
+        user_group = create(:user_group, users: users_in_group)
+
+        expect(User.without_other_user_group(user_group_id: user_group.id)).to match_array(
+          users_in_group + [user_without_group]
+        )
+      end
+    end
+
+    context 'no user group id passed in' do
+      it 'returns all users without a user group' do
+        _user_in_other_user_group = create(:user, user_group: create(:user_group))
+        user_without_group = create(:user)
+
+        expect(User.without_other_user_group(user_group_id: nil)).to match_array(
+          [user_without_group]
+        )
+      end
+    end
+  end
+
   describe '#uuid' do
     it 'assigns uuid on create' do
       user.save
