b9e676efc887b16d8b3aab95bee9cea23d05f456
Nick Bristow <Nick.Bristow@gsa.gov>
2017-04-17 15:16:39 -0400

    Show link when SVG logo is used   SVG's wont load from a different source   Shows link to svg preview when svg is used

--- app/helpers/service_provider_helper.rb
+++ app/helpers/service_provider_helper.rb
@@ -1,7 +1,22 @@
 module ServiceProviderHelper
+  def sp_logo(file_name)
+    file = file_name || 'generic.svg'
+    if file.end_with?('.svg')
+      link_to(file, sp_logo_preview_path(file))
+    else
+      image_tag(sp_logo_path(file))
+    end
+  end
+
   def sp_logo_path(file_name)
     file = file_name || 'generic.svg'
     'https://raw.githubusercontent.com/18F/identity-idp/master/app/assets/images/sp-logos/' +
+      file
+  end
+
+  def sp_logo_preview_path(file_name)
+    file = file_name || 'generic.svg'
+    'https://github.com/18F/identity-idp/blob/master/app/assets/images/sp-logos/' +
       file
   end
 end
--- spec/helpers/service_provider_helper_spec.rb
+++ spec/helpers/service_provider_helper_spec.rb
@@ -10,6 +10,10 @@ describe ServiceProviderHelper do
           /test.png/,
         )
       end
+
+      it 'returns a github raw file url string' do
+        expect(sp_logo_path('test.png')).to eq('https://raw.githubusercontent.com/18F/identity-idp/master/app/assets/images/sp-logos/test.png')
+      end
     end
 
     context 'file name is nil' do
@@ -19,6 +23,41 @@ describe ServiceProviderHelper do
         expect(sp_logo_path(file)).to match(
           /generic.svg/,
         )
+      end
+    end
+  end
+
+  describe '#sp_logo_preview_path' do
+    context 'file name is present' do
+      it 'returns a github preview url string' do
+        expect(sp_logo_preview_path('test.svg')).to eq('https://github.com/18F/identity-idp/blob/master/app/assets/images/sp-logos/test.svg')
+      end
+    end
+  end
+
+  describe '#sp_logo' do
+    context 'file is svg' do
+      it 'returns a link' do
+        file = 'test.svg'
+
+        expect(sp_logo(file)).to eq(link_to(file, sp_logo_preview_path(file)))
+      end
+    end
+
+    context 'file is png' do
+      it 'returns an image tag' do
+        file = 'test.png'
+
+        expect(sp_logo(file)).to eq(image_tag(sp_logo_path(file)))
+      end
+    end
+
+    context 'file is nil' do
+      it 'returns a link to generic.svg' do
+        file = nil
+        default = 'generic.svg'
+
+        expect(sp_logo(file)).to eq(link_to(default, sp_logo_preview_path(default)))
       end
     end
   end
