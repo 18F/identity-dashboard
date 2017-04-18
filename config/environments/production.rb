e75cf3ff141d9943bad5234675f81b6f92106f25
Jessie A. Young <jessie.a.young@gmail.com>
2017-04-18 07:53:05 -0700

    Update all `Figaro.env` instances to be lowercase

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,13 +4,13 @@ Rails.application.configure do
   config.eager_load = true
   config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
-  config.serve_static_files = Figaro.env.SERVE_STATIC_FILES == 'true'
+  config.serve_static_files = Figaro.env.serve_static_files == 'true'
   config.middleware.use Rack::Deflater
   config.assets.js_compressor = :uglifier
   config.assets.compile = false
   config.assets.digest = true
   config.log_level = :debug
-  config.action_controller.asset_host = Figaro.env.ASSET_HOST
+  config.action_controller.asset_host = Figaro.env.asset_host
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = SMTP_SETTINGS
   config.action_mailer.default_url_options = {
@@ -21,4 +21,4 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
   config.active_record.dump_schema_after_migration = false
 end
-Rack::Timeout.timeout = (Figaro.env.RACK_TIMEOUT || 10).to_i
+Rack::Timeout.timeout = (Figaro.env.rack_timeout || 10).to_i
