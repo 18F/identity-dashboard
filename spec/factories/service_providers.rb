0d0f6242c938b3acb48be63790c11ee723607db9
Tom Black <thomas.black@gsa.gov>
2018-03-24 12:42:35 -0700

    Update tests for removed SP `agency` attribute
    **Why**: We removed the SP `agency` field from the views, since it isn't particularly useful for testing. This removes the agencies associated in the SP factory and adds a test to ensure JSON is flowing from the `/api/service_providers` endpoint.

--- spec/factories/service_providers.rb
+++ spec/factories/service_providers.rb
@@ -4,6 +4,5 @@ FactoryGirl.define do
     sequence(:issuer) { |n| "urn:gov:gsa:SAML:2.0.profiles:sp:sso:DEPT:APP-#{n}" }
     sequence(:description) { |n| "test service_provider description #{n}" }
     association :user, factory: :user
-    association :agency, factory: :agency
   end
 end
