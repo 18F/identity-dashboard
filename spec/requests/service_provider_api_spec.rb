f35328f2fffe4797029cc2c2993f6c6e2d386f52
Tom Black <thomas.black@gsa.gov>
2018-03-24 12:42:35 -0700

    Update tests for removed SP `agency` attribute
    **Why**: We removed the SP `agency` field from the views, since it isn't particularly useful for testing. This removes the agencies associated in the SP factory and adds a test to ensure JSON is flowing from the `/api/service_providers` endpoint.

---
+++ spec/requests/service_provider_api_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe 'Service Provider API' do
+  it 'returns JSON' do
+    app = create(:service_provider)
+    get '/api/service_providers'
+    json = JSON.parse(response.body)
+    expect(response).to be_success
+    expect(json[0]["issuer"]).to eq(app.issuer)
+  end
+end
