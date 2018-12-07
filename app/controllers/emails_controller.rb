2cab6ec9e992df99015ae40d3fcada233d59ef91
Steve Urciuoli <steve.urciuoli@gsa.gov>
2018-12-06 23:51:08 -0500

    Lint

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -2,6 +2,6 @@ class EmailsController < ApplicationController
   before_action -> { authorize User }
 
   def index
-    @users = User.all.sort {|a, b| a.domain <=> b.domain}
+    @users = User.all.sort_by(&:domain)
   end
 end
