30b34af5c1aa21b96c64f12a1247cdf2dbef9e5c
Jonathan Hooper <jonathan.hooper@gsa.gov>
2018-12-14 00:25:14 -0600

    Use Omniauth strategy for auth
    **Why**: So we can test-drive the omniauth strategy and take advantage
    of some of the things that it has that are missing from the example SP
    implementation

---
+++ app/controllers/users/omniauth_controller.rb
@@ -0,0 +1,17 @@
+module Users
+  class OmniauthController < ApplicationController
+    def callback
+      omniauth_info = request.env['omniauth.auth']['info']
+      @user = User.find_by_email(omniauth_info[:email])
+      if @user
+        @user.update!(uuid: omniauth_info[:uuid])
+        sign_in @user
+        redirect_to service_providers_path
+
+      # Can't find an account, tell user to contact login.gov team
+      else
+        redirect_to users_none_path
+      end
+    end
+  end
+end
