ccd469ae6a193acbbc01de545dffc2bc361c1c4e
Jonathan Hooper <jonathan.hooper@gsa.gov>
2018-12-14 00:52:54 -0600

    omniauth controller specs

--- app/controllers/users/omniauth_controller.rb
+++ app/controllers/users/omniauth_controller.rb
@@ -2,15 +2,15 @@ module Users
   class OmniauthController < ApplicationController
     def callback
       omniauth_info = request.env['omniauth.auth']['info']
-      @user = User.find_by_email(omniauth_info[:email])
+      @user = User.find_by_email(omniauth_info['email'])
       if @user
-        @user.update!(uuid: omniauth_info[:uuid])
+        @user.update!(uuid: omniauth_info['uuid'])
         sign_in @user
         redirect_to service_providers_path
 
       # Can't find an account, tell user to contact login.gov team
       else
-        redirect_to users_none_path
+        redirect_to users_none_url
       end
     end
   end
