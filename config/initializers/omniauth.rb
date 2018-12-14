36345feb91ebad36578a56f6db98614c214713d1
Jonathan Hooper <jonathan.hooper@gsa.gov>
2018-12-14 00:40:49 -0600

    delint

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,14 +1,13 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :login_dot_gov, {
-    client_id: Rails.configuration.oidc['client_id'],
-    idp_base_url: Rails.configuration.oidc['idp_url'],
-    private_key: OpenSSL::PKey::RSA.new(
-      Figaro.env.saml_sp_private_key,
-      Figaro.env.saml_sp_private_key_password
-    ),
-    redirect_uri: URI.join(
-      Rails.configuration.oidc['dashboard_url'],
-      '/auth/logindotgov/callback'
-    )
-  }
+  provider :login_dot_gov,
+           client_id: Rails.configuration.oidc['client_id'],
+           idp_base_url: Rails.configuration.oidc['idp_url'],
+           private_key: OpenSSL::PKey::RSA.new(
+             Figaro.env.saml_sp_private_key,
+             Figaro.env.saml_sp_private_key_password,
+           ),
+           redirect_uri: URI.join(
+             Rails.configuration.oidc['dashboard_url'],
+             '/auth/logindotgov/callback',
+           )
 end
