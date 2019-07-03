025bdfec05cd837654df66f9d000944e43a35035
Karla Rodriguez <krodriguez.stsi@gmail.com>
2019-07-03 15:13:38 -0400

    adding new files

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,8 @@ Rails.application.config.assets.version = (Figaro.env.assets_version || '1.0')
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.paths << Rails.root.join('node_modules/identity-style-guide/dist/assets')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
