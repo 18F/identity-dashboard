c45fc6b82759577cc56db5d3169a3712e50e5839
Karla Rodriguez <krodriguez.stsi@gmail.com>
2019-07-16 17:15:28 -0400

    Updating form styling with helpers and wrappers

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -1,4 +1,6 @@
 SimpleForm.setup do |config|
+  # Karla Rodriguez changed style from 'btn btn-primary'
+  
   config.button_class = 'btn btn-primary'
   config.boolean_label_class = nil
   config.error_notification_tag = :div
@@ -7,9 +9,31 @@ SimpleForm.setup do |config|
   config.wrappers :base do |b|
     b.use :html5
     b.use :hint,  wrap_with: { tag: :span, class: :hint }
-    b.use :input, class: 'field'
+    # change input class from field to usa-input
+    b.use :input, class: 'usa-input'
+    
   end
 
+  config.wrappers :usa_radio, :tag => 'fieldset', :class => 'usa-fieldset', :error_class => 'error' do |b|
+    b.use :html5
+    b.use :placeholder
+    b.use :label
+    b.wrapper :tag => 'ul', :class => 'usa-input-list' do |input|
+      input.use :input, :wrap_with => { :tag => 'li', :class => 'block-wrapper' }
+      input.use :error, :wrap_with => { :tag => 'span', :class => 'help-inline' }
+      input.use :hint,  :wrap_with => { :tag => 'p', :class => 'help-block' }
+    end
+   end
+   config.wrappers :usa_checkbox, :tag => 'fieldset', :class => 'usa-fieldset', :error_class => 'error' do |b|
+    b.use :html5
+    b.use :placeholder
+    b.use :label
+    b.wrapper :tag => 'ul', :class => 'usa-input-list' do |input|
+      input.use :input, :wrap_with => { :tag => 'li', :class => 'block-wrapper' }
+      input.use :error, :wrap_with => { :tag => 'span', :class => 'help-inline' }
+      input.use :hint,  :wrap_with => { :tag => 'p', :class => 'help-block' }
+    end
+   end 
   config.wrappers :vertical_form, tag: 'div', class: 'mb2', error_class: 'has-error' do |b|
     b.use :html5
     b.use :placeholder
@@ -18,10 +42,13 @@ SimpleForm.setup do |config|
     b.optional :min_max
     b.optional :readonly
     b.use :label
-    b.use :input, class: 'block col-12 field'
+    # changed input class from field to usa-input
+    b.use :input, class: 'block col-12 usa-input'
     b.use :hint,  wrap_with: { tag: 'div', class: :hint }
     b.use :error, wrap_with: { tag: 'div', class: 'error-notify alert-danger p1 mt1' }
   end
 
   config.default_wrapper = :vertical_form
 end
+
+
