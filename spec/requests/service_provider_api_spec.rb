7fe143d14b8e6f8453fd9da1fa261ee26d9f84e1
Doug Price <douglas.price@gsa.gov>
2019-07-17 10:34:04 -0400

    LG-939: alphabetize the list of apps in the dashboard

--- spec/requests/service_provider_api_spec.rb
+++ spec/requests/service_provider_api_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'Service Provider API' do
   it 'returns JSON' do
     app = create(:service_provider)
-    get '/api/service_providers'
+    get api_service_providers_path
     json = JSON.parse(response.body)
     expect(response).to be_success
     expect(json[0]['issuer']).to eq(app.issuer)
