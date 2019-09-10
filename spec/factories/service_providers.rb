d3965529df0529cd3b6dfcd8be491e6924f14401
Jenny Wagner <jennifer.wagner@gsa.gov>
2019-09-10 14:00:26 -0400

    LG-1815 Generate yaml blob for sp (#238)
    * LG-1815 Generate yaml blob
    
    * LG-1815 restrict to admins
    
    * LG-1815 improve formatting to match other service_provider.yml entries
    
    * LG-1815 fix redirect_uri formatting
    
    * LG-1815: hide the yaml row unless admin

--- spec/factories/service_providers.rb
+++ spec/factories/service_providers.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
     sequence(:issuer) { |n| "urn:gov:gsa:SAML:2.0.profiles:sp:sso:DEPT:APP-#{n}" }
     sequence(:description) { |n| "test service_provider description #{n}" }
     association :user, factory: :user
+    association :agency, factory: :agency
 
     trait :saml do
       identity_protocol 1
