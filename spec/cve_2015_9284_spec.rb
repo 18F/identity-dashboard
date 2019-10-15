d4f5b360e0a9ccbba2be8903284eb9177f735d1a
Doug Price <douglas.price@gsa.gov>
2019-10-15 17:00:03 -0400

    LG-1947: mitigation for CVE-2015-9284

---
+++ spec/cve_2015_9284_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+# Make sure that https://nvd.nist.gov/vuln/detail/CVE-2015-9284 is mitigated
+RSpec.describe 'CVE-2015-9284', type: :request do
+  describe 'POST /auth/:provider without CSRF token' do
+    before do
+      @allow_forgery_protection = ActionController::Base.allow_forgery_protection
+      ActionController::Base.allow_forgery_protection = true
+    end
+
+    it do
+      expect do
+        post '/auth/logindotgov'
+      end.to raise_error(ActionController::InvalidAuthenticityToken)
+    end
+
+    after do
+      ActionController::Base.allow_forgery_protection = @allow_forgery_protection
+    end
+  end
+end
