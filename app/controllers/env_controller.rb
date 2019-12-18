5b3216e3d0023bb2a7d8e47185fdca1c4800acd2
Tom Black <thomas.black@gsa.gov>
2019-12-18 17:01:54 -0600

    Move "env status" off of home page
    Partners are confused by the current "environments status" on the home page, namely suggesting that they can integrate with multiple login.gov environments. This creates unnecessary support issues. The "environments status" page is still useful, so this commit moves it to a new `/env` path.

---
+++ app/controllers/env_controller.rb
@@ -0,0 +1,11 @@
+class EnvController < ApplicationController
+  def index
+    @deploy_statuses = deploy_status_checker.check!
+  end
+
+  protected
+
+  def deploy_status_checker
+    @deploy_status_checker ||= DeployStatusChecker.new
+  end
+end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,4 @@
 class HomeController < ApplicationController
   def index
-    @deploy_statuses = deploy_status_checker.check!
-  end
-
-  protected
-
-  def deploy_status_checker
-    @deploy_status_checker ||= DeployStatusChecker.new
   end
 end
