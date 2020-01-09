7f14c0e3690600f9d23b7b9646a8540da73b1d3a
Tom Black <thomas.black@gsa.gov>
2020-01-09 11:55:10 -0600

    Add env controller spec

---
+++ spec/controllers/env_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe EnvController do
+  include Devise::Test::ControllerHelpers
+
+  let(:user) { create(:user) }
+
+  before do
+    allow(controller).to receive(:current_user).and_return(user)
+  end
+
+  describe '#index' do
+    it 'has a success response' do
+      get :index
+      expect(response.status).to eq(200)
+    end
+  end
+end
