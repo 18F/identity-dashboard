78254931fd8f7f094f8792dd8c6f9cdb9fadebac
Doug Price <douglas.price@gsa.gov>
2020-01-21 15:47:27 -0500

    LG-2426, LG-2446: point dashboard to cloud.gov deployed sample SP's

--- spec/services/deploy_status_checker_spec.rb
+++ spec/services/deploy_status_checker_spec.rb
@@ -77,9 +77,15 @@ RSpec.describe DeployStatusChecker do
         to_return(body: status_json.to_json)
       stub_request(:get, %r{https://.*\.int\.identitysandbox\.gov/api/deploy\.json}).
         to_return(body: status_json.to_json)
+      stub_request(:get, %r{https://int-.*\.cloud\.gov/api/deploy\.json}).
+        to_return(body: status_json.to_json)
       stub_request(:get, %r{https://.*\.dev\.identitysandbox\.gov/api/deploy\.json}).
         to_return(status: 404)
+      stub_request(:get, %r{https://dev-.*\.cloud\.gov/api/deploy\.json}).
+        to_return(status: 404)
       stub_request(:get, %r{https://.*\.qa\.identitysandbox\.gov/api/deploy\.json}).
+        to_timeout
+      stub_request(:get, %r{https://qa-.*\.cloud\.gov/api/deploy\.json}).
         to_timeout
     end
 
