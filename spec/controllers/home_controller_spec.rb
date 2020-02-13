c69636a1f69bcefeabc68217dbb085c622e800f4
Amos <amos@gsa.gov>
2020-02-13 11:29:43 -0800

    Redirects root route to SP path for logged in users (#281)
    Not worried about the minor cruft on the spec since it will likely be short-lived as we improve the UX here. bugfix! 🦀

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe HomeController do
+  include Devise::Test::ControllerHelpers
+
+  let(:user) { create(:user) }
+  let(:org) { create(:team) }
+
+  before do
+    allow(controller).to receive(:current_user).and_return(user)
+    sign_in user
+  end
+
+  describe '#index' do
+    context 'when the user is signed in' do
+      it 'has a redirect response' do
+        get :index
+        expect(response.status).to eq(302)
+      end
+    end
+
+    context 'when the user is not signed in' do
+      before do
+        allow(controller).to receive(:current_user).and_return(nil)
+        sign_out user
+      end
+
+      it 'has a success response' do
+        get :index
+        expect(response.status).to eq(200)
+      end
+    end
+  end
+end
