7f326c72dcdb22adfb2c537c3c6fa80b7e4d6ce4
Doug Price <douglas.price@gsa.gov>
2020-03-08 13:13:48 -0400

    LG-2751: Upgrade dashboard to Rails 5.2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,21 +1,38 @@
 Rails.application.configure do
-  config.cache_classes = false
-  config.eager_load = false
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
-  config.after_initialize do
-    Bullet.enable = true
-    Bullet.bullet_logger = true
-    Bullet.rails_logger = true
-  end
-  config.active_support.deprecation = :log
-  config.active_record.migration_error = :page_load
-  config.assets.debug = true
-  config.assets.digest = true
-  config.assets.raise_runtime_errors = true
-  config.action_view.raise_on_missing_translations = true
+  # Settings specified here will take precedence over those in config/application.rb.
 
-  # mail
+  # In the development environment your application's code is reloaded on
+  # every request. This slows down response time but is perfect for development
+  # since you don't have to restart the web server when you make code changes.
+  config.cache_classes = false
+
+  # Do not eager load code on boot.
+  config.eager_load = false
+
+  # Show full error reports.
+  config.consider_all_requests_local = true
+
+  # Enable/disable caching. By default caching is disabled.
+  # Run rails dev:cache to toggle caching.
+  if Rails.root.join('tmp', 'caching-dev.txt').exist?
+    config.action_controller.perform_caching = true
+
+    config.cache_store = :memory_store
+    config.public_file_server.headers = {
+      'Cache-Control' => "public, max-age=#{2.days.to_i}"
+    }
+  else
+    config.action_controller.perform_caching = false
+
+    config.cache_store = :null_store
+  end
+
+  # Store uploaded files on the local file system (see config/storage.yml for options)
+  config.active_storage.service = :local
+
+  # Don't care if the mailer can't send.
+  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'localhost:3001' }
@@ -24,6 +41,43 @@ Rails.application.configure do
     port: Figaro.env.smtp_port || 2025,
   }
   config.action_mailer.perform_deliveries = true
+
+  # Print deprecation notices to the Rails logger.
+  config.active_support.deprecation = :log
+
+  # Raise an error on page load if there are pending migrations.
+  config.active_record.migration_error = :page_load
+
+  # Highlight code that triggered database queries in logs.
+  config.active_record.verbose_query_logs = true
+
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
+  config.assets.debug = true
+
+  # Suppress logger output for asset requests.
+  config.assets.quiet = true
+
+  # Previous settings that may not be relevant anymore
+  config.assets.digest = true
+  config.assets.raise_runtime_errors = true
+
+  # Raises error for missing translations
+  config.action_view.raise_on_missing_translations = true
+
+  # Use an evented file watcher to asynchronously detect changes in source code,
+  # routes, locales, etc. This feature depends on the listen gem.
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Custom for Login.gov
+
+  # Set up Bullet to help kill N+1 queries and unused eager loading
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.bullet_logger = true
+    Bullet.rails_logger = true
+  end
 
   $stdout.sync = true
 end
