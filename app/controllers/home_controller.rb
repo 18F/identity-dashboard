25f042a8e0b83f71b0aca3c876f0930d4649e286
Jenny Wagner <jennywagg@gmail.com>
2020-03-17 15:35:18 -0400

    LG-2471 Update authenticated user homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,14 @@
 class HomeController < ApplicationController
   def index
-    redirect_to teams_path if user_signed_in?
+    render :index and return unless user_signed_in?
+
+    includes = %i[users service_providers agency]
+    @teams = if current_user.admin?
+               Team.includes(*includes).all
+             else
+               current_user.teams.includes(*includes).all
+             end
+
+    render 'home/authenticated/index'
   end
 end
