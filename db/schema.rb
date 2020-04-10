f4d0a8d08182374aaf52901412e2ec527a76e1cd
Karla Rodriguez <karla.rodriguez@gsa.gov>
2020-04-10 17:16:58 -0400

    Add margin to radio buttons to separate from delete button

--- db/schema.rb
+++ db/schema.rb
@@ -67,16 +67,6 @@ ActiveRecord::Schema.define(version: 2020_03_12_022438) do
     t.index ["name"], name: "index_groups_on_name", unique: true
   end
 
-  create_table "help_texts", force: :cascade do |t|
-    t.bigint "service_provider_id"
-    t.json "sign_in", default: {}
-    t.json "sign_up", default: {}
-    t.json "forgot_password", default: {}
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["service_provider_id"], name: "index_help_texts_on_service_provider_id"
-  end
-
   create_table "service_providers", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "issuer", null: false
