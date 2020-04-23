c877402607e7a449fa72f93be9d8ae90fdbff2e0
Doug Price <douglas.price@gsa.gov>
2020-04-23 16:10:45 -0400

    LG-2937: bin/activate will download application.yml from S3 and merge in

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,7 @@ require 'webmock/rspec'
 # http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   config.color = true
+  config.formatter = :documentation
 
   config.expect_with :rspec do |expectations|
     expectations.syntax = :expect
