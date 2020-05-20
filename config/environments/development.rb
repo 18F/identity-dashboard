83f1564946d164ba7a322c3557fe214706afc9b7
Doug Price <douglas.price@gsa.gov>
2020-05-19 20:54:12 -0400

    LG-2960: allow uploads to happen in int

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  logo_enabled = ENV.fetch('logo_upload_enabled', 'true') == 'true'
+  logo_enabled = Figaro.env.logo_upload_enabled == 'true'
   config.active_storage.service = logo_enabled ? :amazon : :local
   # Allow SVG's only because we will always serve them in an <img> element
   config.active_storage.content_types_to_serve_as_binary -= ['image/svg+xml']
