024ad2d661d32400304015a77273c0e94aa977b3
Doug Price <douglas.price@gsa.gov>
2020-05-28 15:38:58 -0400

    downcase the submitted email array to clean up the rest of the code

--- app/forms/manage_users_form.rb
+++ app/forms/manage_users_form.rb
@@ -11,7 +11,8 @@ class ManageUsersForm
   end
 
   def submit(user_emails:)
-    @user_emails = user_emails
+    # Devise makes emails case insensitive by downcasing prior to saving
+    @user_emails = user_emails.map(&:downcase)
 
     return false unless valid?
 
@@ -27,19 +28,19 @@ class ManageUsersForm
     (existing_users + missing_users).sort_by(&:email)
   end
 
+  def missing_emails
+    existing_emails = existing_users.map(&:email)
+    user_emails.filter { |email| !existing_emails.include? email }
+  end
+
+  def existing_users
+    User.where(email: user_emails).to_a
+  end
+
   def user_emails_are_valid_email_addresses
     user_emails.each do |email|
       next if email.match(Devise.email_regexp)
       errors.add(:base, "#{email} is not a valid email address")
     end
-  end
-
-  def existing_users
-    User.where('lower(email) IN (?)', user_emails.map(&:downcase)).to_a
-  end
-
-  def missing_emails
-    existing_emails = existing_users.map { |user| user.email.downcase }
-    user_emails.filter { |email| !existing_emails.include? email.downcase }
   end
 end
