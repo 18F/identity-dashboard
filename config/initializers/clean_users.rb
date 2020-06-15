511901d5ad6984f4aae427ee5f0f57a4b48e3981
Shade L. Jenifer <shade.jenifer@gsa.gov>
2020-06-15 08:05:05 -0400

    Default interval to 30 days if config is missing

--- config/initializers/clean_users.rb
+++ config/initializers/clean_users.rb
@@ -1,6 +1,7 @@
 lambda do
   config = Rails.application.config
   config.after_initialize do
-    CleanUsersJob.new.perform(Figaro.env.clean_users_job_frequency_seconds.to_i)
+    interval = Figaro.env.clean_users_job_frequency_seconds || 30.days
+    CleanUsersJob.new.perform(interval.to_i)
   end
 end.call
