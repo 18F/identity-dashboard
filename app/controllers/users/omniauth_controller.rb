5cdeb26ca45ac34e2507a3ce2d119f7ed519b82c
Mitchell Henke <mitchell.henke@gsa.gov>
2020-07-28 16:43:14 -0500

    Fix branch condition size

--- app/controllers/users/omniauth_controller.rb
+++ app/controllers/users/omniauth_controller.rb
@@ -6,11 +6,15 @@ module Users
 
       if @user
         sign_in @user
-        session[:id_token] = request.env.dig('omniauth.auth', 'credentials', 'id_token')
+        set_id_token
         redirect_to session[:requested_url]
       else
         redirect_to users_none_url
       end
+    end
+
+    def set_id_token
+      session[:id_token] = request.env.dig('omniauth.auth', 'credentials', 'id_token')
     end
   end
 end
