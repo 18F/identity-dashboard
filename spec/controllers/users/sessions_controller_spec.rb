03ce95a6cb4da57e05bc5699827db77c3d422393
Mitchell Henke <mitchell.henke@gsa.gov>
2020-07-30 12:07:17 -0500

    Test logout

---
+++ spec/controllers/users/sessions_controller_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+describe Users::SessionsController do
+  include Devise::Test::ControllerHelpers
+
+  describe '#destroy' do
+    before do
+      request.env["devise.mapping"] = Devise.mappings[:user]
+    end
+
+    let(:user) { create(:user) }
+    let(:uuid) { '123-asdf-qwerty' }
+    let(:omniauth_hash) do
+      {
+        'info' => {
+          'email' => user.email,
+          'uuid' => uuid,
+        },
+        'credentials' => {
+          'id_token'=> 'abc123'
+        }
+      }
+    end
+    let(:logout_utility) { OmniAuth::LoginDotGov::LogoutUtility.new(end_session_endpoint: 'http://localhost:3000') }
+
+    context 'when logged in' do
+      before do
+        session[:id_token] = '33939'
+        allow(controller).to receive(:current_user).and_return(user)
+        allow(controller.class).to receive(:logout_utility).and_return(logout_utility)
+        sign_in user
+      end
+
+      it 'logs user out' do
+        get :destroy
+        expect(response.location).to match('http://localhost:3000')
+        expect(response.status).to eq 302
+      end
+    end
+
+    context 'when logged out' do
+      it 'redirects to the empty user path' do
+        get :destroy
+        expect(response).to redirect_to(root_path)
+      end
+    end
+  end
+end
