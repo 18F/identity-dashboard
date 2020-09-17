b566edcc4c8a8f693bfa32a98094291c75a06ac7
Zach Margolis <zachary.margolis@gsa.gov>
2020-09-17 12:22:40 -0700

    Update how we seed agencies to get around id/name DB unique constraints
    - Hopefully fixes #375

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,22 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Rails.application.config.agencies.each do |agency_id, values|
-  agency = Agency.find_by(id: agency_id)
-  if agency
-    agency.update!(name: values['name'])
-  else
-    Agency.create!(name: values['name'])
+# There are unique indexes on both agency name and agency id so we can't in-place update individual
+# records without potentially causing unique constraint errors. What we do to get around this
+# is to delete the ones we're going to update, and then re-create them.
+#
+# We do this all inside a transaction to provide continuity for other things that read the table.
+Agency.transaction do
+  Agency.where(
+    id: Rails.application.config.agencies.map { |id, _values| id },
+  ).delete_all
+
+  Agency.where(
+    name: Rails.application.config.agencies.map { |_id, values| values['name'] },
+  ).delete_all
+
+  Rails.application.config.agencies.each do |agency_id, values|
+    Agency.create(id: agency_id, name: values['name'])
   end
 end
 
