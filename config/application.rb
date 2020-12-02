9a52a425bc9c57fe74a14777eeb0d2731d9df98b
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-12-02 13:18:22 -0600

    Revert

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ Bundler.require(*Rails.groups)
 module IdentityDashboard
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 5.0
     config.assets.unknown_asset_fallback = true
 
     # Settings in config/environments/* take precedence over those specified here.
