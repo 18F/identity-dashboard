d93fcba9bc7d7fb21f1feb631f60b92efa73a1e2
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-12-02 12:37:38 -0600

    5.2 defaults

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,8 @@ Bundler.require(*Rails.groups)
 module IdentityDashboard
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.0
+    config.load_defaults 5.2
+    ActiveRecord::Base.cache_versioning = false
     config.active_record.belongs_to_required_by_default = false
     config.assets.unknown_asset_fallback = true
 
