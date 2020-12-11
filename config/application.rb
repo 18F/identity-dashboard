50b27ba70de96ba5725e6f994f590d1ec32a7522
Steve Urciuoli <steve.urciuoli@gsa.gov>
2020-12-11 13:50:53 -0600

    LG-3771 Upgrade to Rails 6 (#378)

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 # require "action_cable/engine"
-require "sprockets/railtie"
+# require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -21,6 +21,7 @@ module IdentityDashboard
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.0
+    config.assets.unknown_asset_fallback = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
