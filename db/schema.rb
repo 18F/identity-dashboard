1dbb6e82f2394fef5b79483fb688eb2e6ba2fdb6
Zach Margolis <zachary.margolis@gsa.gov>
2021-02-25 17:10:52 -0800

    bundle exec rails active_storage:update

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_22_185518) do
+ActiveRecord::Schema.define(version: 2021_02_26_010924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,17 +33,24 @@ ActiveRecord::Schema.define(version: 2021_01_22_185518) do
     t.bigint "byte_size", null: false
     t.string "checksum", null: false
     t.datetime "created_at", null: false
+    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "agencies", force: :cascade do |t|
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "agencies", id: :serial, force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["name"], name: "index_agencies_on_name", unique: true
   end
 
-  create_table "delayed_jobs", force: :cascade do |t|
+  create_table "delayed_jobs", id: :serial, force: :cascade do |t|
     t.integer "priority", default: 0, null: false
     t.integer "attempts", default: 0, null: false
     t.text "handler", null: false
@@ -58,7 +65,7 @@ ActiveRecord::Schema.define(version: 2021_01_22_185518) do
     t.index ["priority", "run_at"], name: "delayed_jobs_priority"
   end
 
-  create_table "groups", force: :cascade do |t|
+  create_table "groups", id: :serial, force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name", null: false
@@ -67,17 +74,7 @@ ActiveRecord::Schema.define(version: 2021_01_22_185518) do
     t.index ["name"], name: "index_groups_on_name", unique: true
   end
 
-  create_table "help_texts", force: :cascade do |t|
-    t.bigint "service_provider_id"
-    t.json "sign_in", default: {}
-    t.json "sign_up", default: {}
-    t.json "forgot_password", default: {}
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["service_provider_id"], name: "index_help_texts_on_service_provider_id"
-  end
-
-  create_table "service_providers", force: :cascade do |t|
+  create_table "service_providers", id: :serial, force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "issuer", null: false
     t.string "friendly_name", null: false
@@ -111,7 +108,7 @@ ActiveRecord::Schema.define(version: 2021_01_22_185518) do
     t.index ["issuer"], name: "index_service_providers_on_issuer", unique: true
   end
 
-  create_table "user_groups", force: :cascade do |t|
+  create_table "user_groups", id: :serial, force: :cascade do |t|
     t.integer "user_id"
     t.integer "group_id"
     t.datetime "created_at", null: false
@@ -120,7 +117,7 @@ ActiveRecord::Schema.define(version: 2021_01_22_185518) do
     t.index ["user_id"], name: "index_user_groups_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", id: :serial, force: :cascade do |t|
     t.string "uuid"
     t.string "email", null: false
     t.string "first_name"
@@ -152,6 +149,7 @@ ActiveRecord::Schema.define(version: 2021_01_22_185518) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "service_providers", "agencies"
   add_foreign_key "service_providers", "groups"
 end
