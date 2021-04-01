71906a48668c9bcb2176163f698fe68543625e8d
Zach Margolis <zachary.margolis@gsa.gov>
2021-04-01 10:36:47 -0700

    Add migration to copy singular value to array

---
+++ db/migrate/20210401171237_move_saml_client_cert_to_certs.rb
@@ -0,0 +1,9 @@
+class MoveSAMLClientCertToCerts < ActiveRecord::Migration[6.1]
+  def up
+    ActiveRecord::Base.connection.execute <<-SQL
+      UPDATE service_providers
+      SET certs = (CAST(saml_client_cert AS character varying) || certs)
+      WHERE saml_client_cert <> ''
+    SQL
+  end
+end
