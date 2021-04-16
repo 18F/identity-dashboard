c9ed99581d02c206066e4400469b179669f7e415
Zach Margolis <zachary.margolis@gsa.gov>
2021-04-16 11:28:05 -0700

    Use an in-memory cache store in production
    **Why**: the default is a file store, and we don't have
    permission to write to the tmp directory it uses

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,8 +57,7 @@ Rails.application.configure do
 
   config.log_level = :info
 
-  # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  config.cache_store = :memory_store, { size: 64.megabytes }
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
