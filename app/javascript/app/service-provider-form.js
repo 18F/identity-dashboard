e2d77d53dc429bff5323459d35e0d3219e18a8de
Russ Beye <russell.beye@gsa.com>
2021-09-01 13:49:17 -0500

    LG-5014 General code cleanup, refactoring

--- app/javascript/app/service-provider-form.js
+++ app/javascript/app/service-provider-form.js
@@ -1,89 +1,93 @@
-// Note: We've temporarily disabled validation of the issuer. Calls to
-// `update_issuer` have been commented out.
-
-var SERVICE_PROVIDER_ISSUER_TEMPLATE = 'urn:gov:gsa:{protocol}.profiles:sp:sso:{department}:{app}'
-
 $(function(){
   if(!$('.service-provider-form').length){
     return;
   }
 
-  var toggle_form_fields = function(id_protocol){
-    switch(id_protocol){
+  // Selectors
+  const samlFields = $('.saml-fields');
+  const oidcFields = $('.oidc-fields');
+  const idProtocol = $('input[name="service_provider[identity_protocol]"]');
+  const ialLevel = $('#service_provider_ial');
+  const ialAttributes = $('.ial-attr-wrapper')
+  const failureToProofInput = $('.service_provider_failure_to_proof_url');
+  const redirectURI = $("#add-redirect-uri-field");
+  const pemInputMessage = $('.js-pem-input-error-message');
+  const fileInput = $('.input-file');
+  const filePreview = $('.input-preview');
+  const pemInput = $('.js-pem-input');
+
+  ialAttributes.each((idx, el) => console.log(el))
+
+  // Functions
+  const toggleFormFields = (idProtocol) => {
+    switch(idProtocol) {
       case 'openid_connect_private_key_jwt':
-        $('.saml-fields').hide();
-        $('.oidc-fields').show();
-        break;
       case 'openid_connect_pkce':
-        $('.saml-fields').hide();
-        $('.oidc-fields').show();
+        samlFields.hide();
+        oidcFields.show();
         break;
       case 'saml':
-        $('.saml-fields').show();
-        $('.oidc-fields').hide();
+        samlFields.show();
+        oidcFields.hide();
         break;
       default:
-        $('.saml-fields').show();
-        $('.oidc-fields').show();
+        samlFields.show();
+        oidcFields.show();
     }
   }
 
-  var id_protocol = function(){
-    return $('input[name="service_provider[identity_protocol]"]:checked').val();
+  const toggleIALOptions = (ial) => {
+    switch(ial) {
+      case '1':
+        failureToProofInput.hide();
+        break;
+      case '2':
+        failureToProofInput.show();
+        break;
+      default:
+        failureToProofInput.show();
+    }
   }
 
-  toggle_form_fields(id_protocol());
+  const setPemError = message => pemInputMessage.innerText = message;
 
-  $('input[name="service_provider[identity_protocol]"]').click(function(){
-    toggle_form_fields(id_protocol());
-  });
+  // Page initialization
+  toggleFormFields(idProtocol.val());
+  toggleIALOptions(ialLevel.val());
 
-  // $('input[name="service_provider[issuer_department]"]').keyup(update_issuer);
-  // $('input[name="service_provider[issuer_app]"]').keyup(update_issuer);
+  // Event triggers
+  idProtocol.change(evt => toggleFormFields(evt.target.value));
 
-  // Add another Redirect URI
-  $("#add-redirect-uri-field").click(function() {
-    $(".service_provider_redirect_uris input:last-child").clone().appendTo(".service_provider_redirect_uris");
-  });
+  ialLevel.change(evt => toggleIALOptions(evt.target.value));
+
+  redirectURI.click(() => $(".service_provider_redirect_uris input:last-child")
+      .clone()
+      .appendTo(".service_provider_redirect_uris")
+  );
 
   // This will need to change if we start uploading more files, e.g. certs, etc.
-  const input = document.querySelector('.input-file');
-  const preview = document.querySelector('.input-preview');
-  if (input) {
-    input.addEventListener('change', function () {
-      logo_file = input.files[0];
-      preview.textContent = logo_file.name;
-    });
-  }
+  fileInput.change(() => {
+    const logo_file = filePreview.files[0];
+    filePreview.textContent = logo_file.name;
+  });
 
-  /**
-   * @param {string?} message
-   */
-  function setPemError(message) {
-    var pemInputMessage = document.querySelector('.js-pem-input-error-message');
-    pemInputMessage.innerText = message;
-  }
+  pemInput.change((event) => {
+    const file = event.target.files[0];
 
-  var pemInput = document.querySelector('.js-pem-input');
-  if (pemInput) {
-    pemInput.addEventListener('change', function(event) {
-      var file = event.target.files[0];
+    $('.js-pem-file-name').innerText = file ? file.name : null;
 
-      document.querySelector('.js-pem-file-name').innerText = file ? file.name : null;
-
-      if (file && file.text) {
-        file.text().then(function(content) {
-          if (content.includes('PRIVATE')) {
-            setPemError('This is a private key, upload the public key instead');
-          } else if (!content.includes('-----BEGIN CERTIFICATE-----')) {
-            setPemError('This file does not appear to be PEM encoded');
-          } else {
-            setPemError(null);
-          }
-        });
-      } else {
-        setPemError(null);
-      }
-    });
-  }
+    if (file && file.text) {
+      file.text().then((content) => {
+        if (content.includes('PRIVATE')) {
+          setPemError('This is a private key, upload the public key instead');
+        } else if (!content.includes('-----BEGIN CERTIFICATE-----')) {
+          setPemError('This file does not appear to be PEM encoded');
+        } else {
+          setPemError(null);
+        }
+      });
+    } else {
+      setPemError(null);
+    }
+  });
 });
