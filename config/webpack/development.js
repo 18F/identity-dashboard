537477ff904d3d1527a4f4001a95f7a96648a3f7
Jonathan Hooper <jonathan.hooper@gsa.gov>
2022-05-20 16:59:45 -0400

    webpaaaaaaack!!!!

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,4 +2,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 const environment = require('./environment');
 
+environment.output.filename = "js/[name]-[hash].js"
+
 module.exports = environment;
