ab6d145ed3a9352bff7246b54bb38fc804ef8424
Jonathan Hooper <jonathan.hooper@gsa.gov>
2022-05-23 10:12:10 -0400

    missing semicolon

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,6 +2,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 const environment = require('./environment');
 
-environment.output.filename = "js/[name]-[hash].js"
+environment.output.filename = "js/[name]-[hash].js";
 
 module.exports = environment;
