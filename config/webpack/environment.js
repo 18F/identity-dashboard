b177217634734ab94c9d36d7de5c8487d4f894c7
Zach Margolis <zachary.margolis@gsa.gov>
2022-06-09 15:30:40 -0700

    Revert "Remove jQuery from dep list (#556)"
    This reverts commit 95683a490505f853a088408774f1ed32aeb08456.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,25 @@
 const { environment } = require('@rails/webpacker');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
 
-module.exports = environment.toWebpackConfig();
+module.exports = environment;
+
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    jquery: 'jquery'
+  })
+);
+
+const envConfig = environment;
+const aliasConfig = {
+  resolve: {
+    alias: {
+      jquery: 'jquery/src/jquery'
+    }
+  }
+};
+
+module.exports = merge(envConfig.toWebpackConfig(), aliasConfig);
