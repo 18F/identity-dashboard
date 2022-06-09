0acd53c128d3c6c3b1977a985e77dca0d2487937
Jonathan Hooper <jonathan.hooper@gsa.gov>
2022-06-09 16:13:47 -0400

    Get rid of application.js (#557)
    The only thing that app.js did was included the service provider JS on every page and include the utils.js which is now gone. This commit only includes the service provider js where necessary and removes application.js.

--- app/javascript/packs/service-provider-form.js
+++ app/javascript/packs/service-provider-form.js
@@ -132,6 +132,10 @@ function logoUploadSetup() {
   const logoInput = document.querySelector('.logo-input-file');
   const logoPreview = document.querySelector('.input-preview');
 
+  if (!logoInput) {
+    return;
+  }
+
   // Functions
   const handleUploadedLogo = () => {
     const logoFile = logoInput.files[0];
@@ -161,10 +165,6 @@ function redirectURISetup() {
 }
 
 function serviceProviderForm() {
-  if (!document.querySelector('.service-provider-form')) {
-    return;
-  }
-
   ialOptionSetup();
   protocolOptionSetup();
   certificateUploadSetup();
