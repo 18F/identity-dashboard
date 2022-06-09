8f91b7b6ad89b9b9fa7d8f4a09f2b2daaeb35c42
Jonathan Hooper <jonathan.hooper@gsa.gov>
2022-06-09 12:02:10 -0400

    Remove jQuery from dep list (#556)
    * Remove active session check and timeout logic
    
    I was trying to convert the JS for this timeout modal from jQuery to vanilla JS. I could not figure out where any of it was used so I've just removed it in this commit. After a bit of digging around in the history I'm pretty confident this code has never actually been run.
    
    This commit gets rid of JQuery since previous work replaced the code that depended on it with plain old-fashioned JS.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,25 +1,3 @@
 const { environment } = require('@rails/webpacker');
-const merge = require('webpack-merge');
-const webpack = require('webpack');
 
-module.exports = environment;
-
-environment.plugins.prepend(
-  'Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    jquery: 'jquery'
-  })
-);
-
-const envConfig = environment;
-const aliasConfig = {
-  resolve: {
-    alias: {
-      jquery: 'jquery/src/jquery'
-    }
-  }
-};
-
-module.exports = merge(envConfig.toWebpackConfig(), aliasConfig);
+module.exports = environment.toWebpackConfig();
