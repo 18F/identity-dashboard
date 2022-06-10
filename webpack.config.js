e4e2ba07204b919ad71d98fd0e8d1941bda491b1
Jonathan Hooper <jonathan.hooper@gsa.gov>
2022-06-10 15:51:27 -0400

    JSBundling setup
    This commit starts the process for setting up JSBundling and dropping @rails/webpacker

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path    = require("path")
+const webpack = require("webpack")
+
+const env = process.env.NODE_ENV || process.env.RAILS_ENV || 'development';
+const isProductionEnv = env === 'production';
+
+module.exports = {
+  mode: isProductionEnv ? 'production' : 'development',
+  devtool: "source-map",
+  entry: {
+    application: "./app/javascript/packs/application.js",
+    manage_users: "./app/javascript/packs/manage_users.js",
+    service_provider_form: "./app/javascript/packs/service_provider_form.js",
+    validate_logo_size: "./app/javascript/packs/validate_logo_size.js",
+  },
+  output: {
+    filename: `[name].js`,
+    sourceMapFilename: `[name].js.map`,
+    path: path.resolve(__dirname, `app/assets/builds`),
+  },
+  plugins: [
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+}
