1b9a241b0f5c7016370341d6df56b331bd18bad2
Jonathan Hooper <jonathan.hooper@gsa.gov>
2022-06-27 11:14:47 -0400

    glob things

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const { sync: glob } = require('fast-glob');
 const webpack = require("webpack");
 
 const env = process.env.NODE_ENV || process.env.RAILS_ENV || 'development';
@@ -7,12 +8,10 @@ const isProductionEnv = env === 'production';
 module.exports = {
   mode: isProductionEnv ? 'production' : 'development',
   devtool: isProductionEnv ? false : 'eval-source-map',
-  entry: {
-    application: "./app/javascript/packs/application.js",
-    manage_users: "./app/javascript/packs/manage_users.js",
-    service_provider_form: "./app/javascript/packs/service_provider_form.js",
-    validate_logo_size: "./app/javascript/packs/validate_logo_size.js",
-  },
+  entry: glob('app/javascript/packs/*.js').reduce((result, filepath) => {
+    result[path.parse(filepath).name] = path.resolve(filepath);
+    return result;
+  }, {}),
   output: {
     filename: `[name].js`,
     sourceMapFilename: `[name].js.map`,
