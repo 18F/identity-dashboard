04b79c48c9011f2cb84b22f7eae3d88b157b9cca
Michael Ambroselli <michael.ambroselli@gsa.gov>
2022-09-20 12:01:39 -0400

    Add Add New User page

--- app/controllers/teams/users_controller.rb
+++ app/controllers/teams/users_controller.rb
@@ -1,6 +1,28 @@
 class Teams::UsersController < AuthenticatedController
    before_action -> { authorize team, policy_class: TeamUsersPolicy }
 
+    def new
+      @user = User.new
+    end
+  
+    def create
+      add_email = user_params[:email].downcase
+      existing_user_emails = team.users.map(&:email)
+      if existing_user_emails.include?(add_email)
+        @user = User.new
+        flash[:error] = "#{add_email} is already part of the team"
+        render :new
+      else
+        @user = User.where(email: add_email).first || User.new(user_params)
+        if team.update(users: team.users + [@user])
+            flash[:success] = 'Success'
+            redirect_to team_path(@team.id)
+        else
+            render :new
+        end
+      end
+    end
+
     def remove_confirm
       if user_present_not_current_user(user)
         render :remove_confirm      
@@ -27,6 +49,10 @@ class Teams::UsersController < AuthenticatedController
 
     def user_present_not_current_user(user)
       user.present? && user != current_user
+    end
+
+    def user_params
+      params.require(:user).permit(:email)
     end
 
     def team
