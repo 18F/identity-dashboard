7d9c0327f8b85b135fcc05fe665317ff5f4de649
Nadya Primak <nadya.primak@gsa.gov>
2022-10-03 10:54:31 -0400

    fix typo and update which message to show upon remove success

--- app/controllers/teams/users_controller.rb
+++ app/controllers/teams/users_controller.rb
@@ -15,7 +15,7 @@ class Teams::UsersController < AuthenticatedController
       user = team.users.find_by(id: params[:id])
       if user.present?
         team.users.delete(user)
-        flash[:success] = I18n.t('notices.user_deleted', email: user.email)
+        flash[:success] = I18n.t('notices.user_team_deleted', email: user.email)
         redirect_to team_path(@team.id)
       else
         render_401
