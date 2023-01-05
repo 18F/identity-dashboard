cae37d710513c3a464c428c99ff6cc702d876122
Julia Allen <julia.l.allen@gsa.gov>
2023-01-05 12:51:39 -0800

    Push updates

--- app/controllers/teams/users_controller.rb
+++ app/controllers/teams/users_controller.rb
@@ -1,62 +1,81 @@
 class Teams::UsersController < AuthenticatedController
-    before_action -> { authorize team, policy_class: TeamUsersPolicy }
+  before_action -> { authorize team, policy_class: TeamUsersPolicy }
 
-    def new
-      @user = User.new
-    end
-  
-    def create
-      add_email = user_params.require(:email).downcase
-      @user = User.find_or_initialize_by(email: add_email)
-      if team.users.include?(@user)
-        @user = User.new
-        flash[:error] = I18n.t('teams.users.create.already_member', email: add_email)
-      elsif not @user.valid?
-        flash[:error] = @user.errors.of_kind?(:email, :invalid) ?
-                        I18n.t('teams.users.create.invalid_email', email: add_email) : 
-                        @user.errors.objects.first.full_message
-      elsif team.update(users: team.users + [@user])
-        flash[:success] = I18n.t('teams.users.create.success', email: add_email)
-        redirect_to team_path(team.id) and return
-      end
-      render :new
-    end
+  def new
+    @user = User.new
+  end
 
-    def remove_confirm
-      if user_present_not_current_user(user)
-        render :remove_confirm      
-      else
-        render_401
-      end
-    end
-  
-    def destroy
-      if user_present_not_current_user(user)
-        team.users.delete(user)
-        flash[:success] = I18n.t('teams.users.remove.success', email: user.email)
-        redirect_to team_path(team.id)
-      else
-        render_401
-      end
-    end
-  
-    private
+  def create
+    # add_email =
+    # new_member = User.find_or_initialize_by(email: add_email)
+    # if team.users.include?(@user)
+    #   @user = User.new
+    #   flash[:error] = I18n.t('teams.users.create.already_member', email: add_email)
+    # if not @user.valid?
+    #   flash[:error] = @user.errors.of_kind?(:email, :invalid) ?
+    #                   I18n.t('teams.users.create.invalid_email', email: add_email) :
+    #                   @user.errors.objects.first.full_message
+    # if team.update(users: team.users + [@user])
+      # flash[:success] = I18n.t('teams.users.create.success', email: add_email)
+      # redirect_to team_path(team.id) and return
+    # end
+    # begin
+    # require 'pry'
+    # binding.pry
+    new_member.user_teams << UserTeam.create!(user_id: new_member.id, group_id: team.id)
+    new_member.save!
+    flash[:success] = I18n.t('teams.users.create.success', email: member_email)
+    redirect_to team_path(team.id) and return
+  rescue ActiveRecord::RecordInvalid => e
+    flash[:error] = "#{member_email}: " + e.record.errors.messages.values.flatten.pop
+    redirect_to new_team_user_path
+  end
 
-    def user
-      @user ||= team.users.find_by(id: params[:id])
+  def remove_confirm
+    if user_present_not_current_user(user)
+      render :remove_confirm
+    else
+      render_401
     end
+  end
 
-    def user_present_not_current_user(user)
-      user.present? && user != current_user
+  def destroy
+    if user_present_not_current_user(user)
+      team_and_users.users.delete(user)
+      flash[:success] = I18n.t('teams.users.remove.success', email: user.email)
+      redirect_to team_path(team.id)
+    else
+      render_401
     end
+  end
 
-    def user_params
-      params.require(:user).permit(:email)
-    end
+  private
 
-    def team
-      @team ||= Team.includes(:users).find(params[:team_id])
-    end
+  def member_email
+    user_params.require(:email).downcase
+  end
 
+  def new_member
+    @new_member ||= User.find_or_initialize_by(email: member_email)
+  end
+
+  def user
+    @user ||= team.users.find_by(id: params[:id])
+  end
+
+  def user_present_not_current_user(user)
+    user.present? && user != current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:email)
+  end
+
+  def team_and_users
+    @team ||= Team.includes(:users).find(params[:team_id])
+  end
+
+  def team
+    @team ||= Team.find(params[:team_id])
+  end
 end
-  
--- app/models/user_team.rb
+++ app/models/user_team.rb
@@ -5,4 +5,7 @@ class UserTeam < ApplicationRecord
 
   belongs_to :user
   belongs_to :team, foreign_key: 'group_id', inverse_of: :user_teams
+
+  validates_uniqueness_of :user_id, scope: :group_id, on: :create,
+                          :message=> 'This user is already a member of the team.'
 end
