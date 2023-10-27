796739fcfa4ea23432f6af6439afd4687cf819c8
davida marion <davida.marion@gsa.gov>
2023-10-27 15:24:31 -0400

    Refactor tools controller methods into class

---
+++ app/models/tools/saml_request.rb
@@ -0,0 +1,95 @@
+module Tools
+  class SAMLRequest
+    attr_reader :auth_url, :cert_body
+    attr_accessor :errors
+
+    def initialize(params)
+      @auth_url = params['auth_url']
+      @cert_body = params['cert']
+      @errors = []
+    end
+
+    def run_validations
+      valid
+      valid_signature if valid
+    end
+
+    def matching_cert_sn
+      auth_service_provider.matching_cert.serial
+    end
+
+    def xml
+      REXML::Document.new(auth_request.raw_xml)
+    end
+
+    def valid
+      @valid = auth_request.valid? if @valid.nil?
+      @valid
+    end
+
+    def valid_signature
+     @valid_signature = check_signature_validity if @valid_signature.nil?
+     @valid_signature
+    end
+
+    private
+
+    def certs
+      cert_body.blank? ? sp&.certs : [cert_body]
+    end
+
+    def check_signature_validity
+      @valid_signature ||= begin
+
+        if certs.nil?
+          @errors.push('Could not find any certificates to use. Please add a certificate to your application configuration or paste one below.')
+          return false
+        end
+
+        if auth_service_provider.nil?
+          @errors.push("No matching Service Provider founded in this request. Please check issuer attribute.")
+          return false
+        end
+
+        begin
+          auth_service_provider.certs = valid_certs
+        rescue OpenSSL::X509::CertificateError
+          @errors.push('Something is wrong with the certificate you submitted.')
+          return false
+        end
+
+        auth_service_provider.valid_signature?(
+          Saml::XML::Document.parse(auth_request.raw_xml), true, auth_request.options
+        )
+      end
+    end
+
+    def valid_certs
+      certs.map { |cert| OpenSSL::X509::Certificate.new(cert) }
+    end
+
+    def auth_request
+      @auth_request ||= SamlIdp::Request.from_deflated_request(saml_params[:SAMLRequest], get_params: saml_params)
+    end
+
+    def auth_service_provider
+      @auth_service_provider ||= auth_request.service_provider
+    end
+
+    def saml_params
+      @saml_params ||= begin
+        s_params = url_params(auth_url)
+        s_params.present? ? s_params : { SAMLRequest: auth_url }
+      end
+    end
+
+    def sp
+      ServiceProvider.find_by(issuer: auth_request.issuer)
+    end
+
+    def url_params(url)
+      CGI.parse(url.split('?')[1..].join('?')).to_h { |k, v| [ k.to_sym, v[0] ] }
+    end
+
+  end
+end
---
+++ spec/models/saml_request_spec.rb
@@ -0,0 +1,132 @@
+require 'rails_helper'
+
+describe 'SAMLRequest' do
+  let(:cert) { build_pem(serial: 200) }
+  let(:auth_url) { 'auth_url '}
+  let(:params) { { auth_url:, cert: cert }.with_indifferent_access }
+  subject(:request) { Tools::SAMLRequest.new(params) }
+
+  describe '#init' do
+    it 'sets the attributes' do
+      expect(subject.auth_url).to eq auth_url
+      expect(subject.cert_body).to eq cert
+      expect(subject.errors).to eq([])
+    end
+  end
+
+  describe '#valid' do
+    let(:decoded_request) { double SamlIdp::Request }
+    let(:validity) { true }
+    before do
+      allow(SamlIdp::Request).to receive(:from_deflated_request)
+      allow(decoded_request).to receive(:valid?) { validity }
+    end
+
+    describe 'when the request is valid' do
+      it 'returns true' do
+        expect(SamlIdp::Request).to receive(:from_deflated_request).with(
+          auth_url, get_params: {SAMLRequest: auth_url}
+        ) { decoded_request }
+
+        expect(subject.valid).to be true
+      end
+
+      it 'only invokes from_deflated_request once' do
+        expect(SamlIdp::Request).to receive(:from_deflated_request) { decoded_request }.once
+        subject.valid
+        subject.valid
+      end
+    end
+
+    describe 'when the request is not valid' do
+      let(:validity) { false }
+      it 'returns false if' do
+        expect(SamlIdp::Request).to receive(:from_deflated_request).with(
+          auth_url, get_params: {SAMLRequest: auth_url}
+        ) { decoded_request }
+
+        expect(subject.valid).to be false
+      end
+
+      it 'only invokes from_deflated_request once' do
+        expect(SamlIdp::Request).to receive(:from_deflated_request) { decoded_request }.once
+        subject.valid
+        subject.valid
+      end
+    end
+  end
+
+  describe '#valid_signature' do
+    let(:decoded_request) { double SamlIdp::Request }
+    let(:issuer) { '1234' }
+    let(:sp) { SamlIdp::ServiceProvider.new }
+
+    before do
+      allow(SamlIdp::Request).to receive(:from_deflated_request) { decoded_request }
+      allow(decoded_request).to receive(:issuer) { issuer }
+    end
+
+    describe 'if there are no certs' do
+      let(:cert) { '' }
+      let(:issuer) { nil }
+
+      it 'returns false' do
+        expect(subject.valid_signature).to be false
+      end
+
+      it 'adds an error' do
+        subject.valid_signature
+        expect(subject.errors).to eq ['Could not find any certificates to use. Please add a certificate to your application configuration or paste one below.']
+      end
+    end
+
+    describe 'if there is no Service Provider in the auth request' do
+      before do
+        allow(decoded_request).to receive(:service_provider) { nil}
+      end
+
+      it 'returns false' do
+        expect(subject.valid_signature).to be false
+      end
+
+      it 'adds an error' do
+        subject.valid_signature
+        expect(subject.errors).to eq ['No matching Service Provider founded in this request. Please check issuer attribute.']
+      end
+    end
+
+    describe 'if a bad cert is passed in' do
+      let(:cert) { 'i am not a cert!' }
+      before do
+        allow(decoded_request).to receive(:service_provider) { sp }
+      end
+
+      it 'returns false' do
+        expect(subject.valid_signature).to be false
+      end
+
+      it 'adds an error' do
+        subject.valid_signature
+        expect(subject.errors).to eq ['Something is wrong with the certificate you submitted.']
+      end
+    end
+
+    describe 'it is valid' do
+      before do
+        expect(decoded_request).to receive(:service_provider) { sp }
+        expect(decoded_request).to receive(:raw_xml)
+        expect(decoded_request).to receive(:options)
+        expect(sp).to receive(:valid_signature?) { true }
+      end
+
+      it 'returns true' do
+        expect(subject.valid_signature).to be true
+      end
+
+      it 'has no errors' do
+        subject.valid_signature
+        expect(subject.errors).to eq []
+      end
+    end
+  end
+end
