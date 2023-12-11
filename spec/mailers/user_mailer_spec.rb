002d039aa4e76a3f98ba6da1250287f05e4295ca
davida marion <davida.marion@gsa.gov>
2023-12-11 12:23:14 -0500

    Remove unused notify_users method and associated code

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,54 +1,17 @@
 require 'rails_helper'
 
 describe UserMailer, type: :mailer do
-  let(:app) { create(:service_provider) }
+  let(:user) { create(:user) }
 
-  describe 'user_new_service_provider' do
-    let(:mail) { UserMailer.user_new_service_provider(app) }
+  describe 'welcome_new_user' do
+    let(:mail) { UserMailer.welcome_new_user(user) }
 
-    it 'sends to app owner' do
-      expect(mail.to).to eq [app.user.email]
+    it 'sends to user' do
+      expect(mail.to).to eq [user.email]
     end
 
     it 'renders the subject' do
-      expect(mail.subject).to eq t('mailer.new_service_provider.subject', id: app.issuer)
-    end
-  end
-
-  describe 'admin_new_service_provider' do
-    let(:mail) { UserMailer.admin_new_service_provider(app) }
-
-    it 'sends to admin_email' do
-      allow(IdentityConfig.store).to receive(:admin_email).and_return('identity-admin@example.com')
-    end
-
-    it 'renders the subject' do
-      expect(mail.subject).to eq t('mailer.new_service_provider.subject', id: app.issuer)
-    end
-  end
-
-  describe 'user_approved_service_provider' do
-    let(:mail) { UserMailer.user_approved_service_provider(app) }
-
-    it 'sends to app owner' do
-      expect(mail.to).to eq [app.user.email]
-    end
-
-    it 'renders the subject' do
-      expect(mail.subject).to eq t('mailer.approved_service_provider.subject', id: app.issuer)
-    end
-  end
-
-  describe 'admin_approved_service_provider' do
-    let(:mail) { UserMailer.admin_approved_service_provider(app) }
-
-    it 'sends to ADMIN_EMAIL' do
-      allow(IdentityConfig.store).to receive(:admin_email).and_return('identity-admin@example.com')
-      expect(mail.to).to eq ['identity-admin@example.com']
-    end
-
-    it 'renders the subject' do
-      expect(mail.subject).to eq t('mailer.approved_service_provider.subject', id: app.issuer)
+      expect(mail.subject).to eq t('mailer.welcome.subject')
     end
   end
 end
