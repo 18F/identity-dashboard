f6ff3b003b0aa131acf5f8bacc6852f99dae1d91
davida marion <davida.marion@gsa.gov>
2024-02-07 14:15:12 -0500

    Pull test vars into let blocks

--- spec/requests/service_providers_spec.rb
+++ spec/requests/service_providers_spec.rb
@@ -1,65 +1,59 @@
 require 'rails_helper'
 
 describe 'Users::ServiceProviders' do
+  let(:sp) { create(:service_provider, :with_team) }
+  let(:user) { create(:user) }
+  let(:admin_user) { create(:user, admin: true) }
   describe 'approve on update' do
     it 'disallows app owner from approving the app' do
-      app = create(:service_provider)
-      login_as(app.user)
+      login_as(sp.user)
 
-      put service_provider_path(app), params: { service_provider: { approved: 'true' } }
+      put service_provider_path(sp), params: { service_provider: { approved: 'true' } }
 
       expect(response.status).to eq(401)
     end
 
     it 'disallows non owner from approving the app' do
-      app = create(:service_provider)
-      user = create(:user)
       login_as(user)
 
-      put service_provider_path(app), params: { service_provider: { approved: 'true' } }
+      put service_provider_path(sp), params: { service_provider: { approved: 'true' } }
 
       expect(response.status).to eq(401)
     end
 
+
     it 'allows admin to approve' do
-      app = create(:service_provider, :with_team)
-      admin_user = create(:user, admin: true)
       login_as(admin_user)
 
-      put service_provider_path(app), params: { service_provider: { approved: 'true' } }
+      put service_provider_path(sp), params: { service_provider: { approved: 'true' } }
 
       expect(response.status).to eq(302)  # redirect on success
-      app.reload
-      expect(app.approved?).to eq(true)
+      sp.reload
+      expect(sp.approved).to eq(true)
     end
   end
 
   describe 'view an app' do
     it 'allows owner to view' do
-      app = create(:service_provider)
-      login_as(app.user)
+      login_as(sp.user)
 
-      get service_provider_path(app)
+      get service_provider_path(sp)
 
       expect(response.status).to eq(200)
     end
 
     it 'disallows non-owner from viewing' do
-      user = create(:user)
-      app = create(:service_provider)
       login_as(user)
 
-      get service_provider_path(app)
+      get service_provider_path(sp)
 
       expect(response.status).to eq(401)
     end
 
     it 'permits admin to view' do
-      admin_user = create(:user, admin: true)
-      app = create(:service_provider, :with_team)
       login_as(admin_user)
 
-      get service_provider_path(app)
+      get service_provider_path(sp)
 
       expect(response.status).to eq(200)
     end
