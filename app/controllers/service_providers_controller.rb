13f986807b7c47fc2626ef42dc1757bacdb427ba
Jeremy Curcio <jeremy.curcio@gsa.gov>
2024-02-16 12:21:42 -0500

    Make link happy.

--- app/controllers/service_providers_controller.rb
+++ app/controllers/service_providers_controller.rb
@@ -5,11 +5,15 @@ class ServiceProvidersController < AuthenticatedController
   before_action :add_iaa_warning, except: %i[index destroy]
 
   def index
-    prod_apps = current_user.scoped_service_providers.select { |sp| sp.prod_config == true }
-    non_prod_apps = current_user.scoped_service_providers.select { |sp| sp.prod_config == false }
+    prod_apps = current_user.scoped_service_providers.select { 
+                  |sp| sp.prod_config == true 
+                }
+    non_prod_apps = current_user.scoped_service_providers.select { 
+                  |sp| sp.prod_config == false 
+                }
 
-    @service_providers = { "My Production Apps" => prod_apps, 
-                           "My Non-Production Apps" => non_prod_apps }
+    @service_providers = { 'My Production Apps' => prod_apps, 
+                           'My Non-Production Apps' => non_prod_apps }
     @show_created_at = false
   end
 
@@ -54,11 +58,15 @@ class ServiceProvidersController < AuthenticatedController
 
   def all
     return unless current_user.admin?
-    prod_apps = ServiceProvider.all.sort_by(&:created_at).reverse.select { |sp| sp.prod_config == false }
-    non_prod_apps = ServiceProvider.all.sort_by(&:created_at).reverse.select { |sp| sp.prod_config == false }
+    prod_apps = ServiceProvider.all.sort_by(&:created_at).reverse.select { 
+                  |sp| sp.prod_config == false 
+                }
+    non_prod_apps = ServiceProvider.all.sort_by(&:created_at).reverse.select { 
+                  |sp| sp.prod_config == false 
+                }
 
-    @service_providers = { "Production Apps" => prod_apps,
-                           "Non-Production Apps" => non_prod_apps }
+    @service_providers = { 'Production Apps' => prod_apps,
+                           'Non-Production Apps' => non_prod_apps }
     @show_created_at = true
   end
 
