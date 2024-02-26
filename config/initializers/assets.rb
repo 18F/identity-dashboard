c68d47426bf4132b92379df880be736dc7c2674c
davida marion <davida.marion@gsa.gov>
2024-02-26 16:39:15 -0500

    Remove unnecessary stuff

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,4 @@
-# require 'extensions/propshaft/asset'
+# Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
