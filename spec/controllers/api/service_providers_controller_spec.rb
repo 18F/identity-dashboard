083770ed1243605b5f9943315db2b4c052daa112
Jeremy Curcio <jeremy.curcio@gsa.gov>
2024-02-27 08:43:24 -0500

    Move the tests

--- spec/controllers/api/service_providers_controller_spec.rb
+++ spec/controllers/api/service_providers_controller_spec.rb
@@ -56,46 +56,4 @@ describe Api::ServiceProvidersController do
     end
 
   end
-
-  describe '#update' do
-    context 'no user' do
-      it 'requires authentication' do
-        post :update
-
-        expect(response).to redirect_to root_url
-      end
-    end
-
-    context 'with user' do
-      before do
-        user = create(:user)
-        sign_in(user)
-      end
-
-      it 'redirects to service_providers_path' do
-        post :update
-
-        expect(response).to redirect_to service_providers_path
-      end
-
-      context 'when ServiceProviderUpdater fails' do
-        before do
-          stub_request(:post, IdentityConfig.store.idp_sp_url).
-            to_return(status: 404)
-        end
-
-        it 'redirects to service_providers_path' do
-          post :update
-
-          expect(response).to redirect_to service_providers_path
-        end
-
-        it 'notifies NewRelic of the error' do
-          expect(::NewRelic::Agent).to receive(:notice_error)
-
-          post :update
-        end
-      end
-    end
-  end
 end
