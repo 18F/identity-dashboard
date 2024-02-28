f3c1aee313e6bde4b5b61779009f766d9ffa1f32
Davida (she/they) <davida.marion@gsa.gov>
2024-02-28 13:50:52 -0500

    LG-12330 update design system (#740)
    * Update identity-style-guide to 3.0
    included:
    
    - Update application to use propshaft instead of ruby-sass
    - Update scss for code climate
    - Lots of markup and style fixes to adhere to uswds standards
    
    Co-authored-by: Andrew Duthie <1779930+aduth@users.noreply.github.com>

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,39 +1,11 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = IdentityConfig.store.assets_version
+Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
-# Rails.application.config.assets.paths << Emoji.images_path
-Rails.application.config.assets.paths << Rails.root.join('public',
-                                                         'assets',
-                                                         'identity-style-guide',
-                                                         'dist',
-                                                         'assets',
-                                                         'img')
-Rails.application.config.assets.paths << Rails.root.join('node_modules',
-                                                         'identity-style-guide',
-                                                         'dist',
-                                                         'assets')
-Rails.application.config.assets.paths << Rails.root.join('node_modules')
-
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w[ application.css
-                                                  18f-logo.svg
-                                                  logo.svg
-                                                  img/close.svg
-                                                  img/favicons/favicon.ico
-                                                  img/favicons/favicon.png
-                                                  img/favicons/favicon-57.png
-                                                  img/favicons/favicon-72.png
-                                                  img/favicons/favicon-114.png
-                                                  img/favicons/favicon-144.png
-                                                  img/favicons/favicon-192.png
-                                                  img/gsa-logo-rev.svg
-                                                  img/icon-dot-gov.svg
-                                                  img/icon-https.svg
-                                                  img/illustrations/security-key.svg
-                                                  img/us_flag_small.png
-                                                  js/main.js ]
+Rails.application.config.assets.paths.push(
+  'node_modules/identity-style-guide/dist/assets/img',
+  'node_modules/identity-style-guide/dist/assets/fonts',
+  'node_modules/identity-style-guide/dist/assets/js',
+)
--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -8,30 +8,9 @@ SimpleForm.setup do |config|
 
   config.wrappers :base do |b|
     b.use :html5
-    b.use :hint,  wrap_with: { tag: :span, class: :hint }
+    b.use :hint,  wrap_with: { tag: :span, class: 'usa-hint' }
     # change input class from field to usa-input
     b.use :input, class: 'usa-input'
-  end
-
-  config.wrappers :usa_radio, tag: 'fieldset', class: 'usa-fieldset', error_class: 'error' do |b|
-    b.use :html5
-    b.use :placeholder
-    b.use :label
-    b.wrapper tag: 'ul', class: 'usa-input-list' do |input|
-      input.use :input, wrap_with: { tag: 'li',   class: 'block-wrapper' }
-      input.use :error, wrap_with: { tag: 'span', class: 'help-inline' }
-      input.use :hint,  wrap_with: { tag: 'p',    class: 'help-block' }
-    end
-  end
-  config.wrappers :usa_checkbox, tag: 'fieldset', class: 'usa-fieldset', error_class: 'error' do |b|
-    b.use :html5
-    b.use :placeholder
-    b.use :label
-    b.wrapper tag: 'ul', class: 'usa-input-list' do |input|
-      input.use :input, wrap_with: { tag: 'li',   class: 'usa-checkbox__input' }
-      input.use :error, wrap_with: { tag: 'span', class: 'help-inline' }
-      input.use :hint,  wrap_with: { tag: 'p',    class: 'help-block' }
-    end
   end
 
   config.wrappers :vertical_form, tag: 'div', class: 'mb2', error_class: 'has-error' do |b|
