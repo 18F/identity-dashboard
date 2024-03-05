81373346129c64fbcb57eef3cdf4c130504db6bc
davida marion <davida.marion@gsa.gov>
2024-03-05 12:21:40 -0500

    Add uswds-init to reduce flicker effecte

---
+++ app/javascript/packs/uswds-init.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-var */
+(function uswdsInit() {
+  var loadingClass = "usa-js-loading";
+  var fallback;
+
+  document.documentElement.classList.add(loadingClass);
+  function revertClass() {
+    document.documentElement.classList.remove(loadingClass);
+  }
+
+  fallback = setTimeout(revertClass, 8000);
+
+  function verifyLoaded() {
+    if (window.uswdsPresent) {
+      clearTimeout(fallback);
+      revertClass();
+      window.removeEventListener("load", verifyLoaded, true);
+    }
+  }
+
+  window.addEventListener("load", verifyLoaded, true);
+}());
