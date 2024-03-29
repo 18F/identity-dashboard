49336340647d0b00218d72d8baf65a98680633db
davida marion <davida.marion@gsa.gov>
2024-03-29 13:30:01 -0400

    Add _html to i18n strings with html

--- spec/features/service_providers_spec.rb
+++ spec/features/service_providers_spec.rb
@@ -50,7 +50,7 @@ feature 'Service Providers CRUD' do
 
       visit service_provider_path(service_provider)
 
-      expect(page).to have_content(strip_tags(I18n.t('service_provider_form.saml_redirects')))
+      expect(page).to have_content(strip_tags(I18n.t('service_provider_form.saml_redirects_html')))
       expect(page).to have_content(I18n.t('service_provider_form.saml_assertion_encryption'))
     end
 
@@ -61,7 +61,7 @@ feature 'Service Providers CRUD' do
 
       visit service_provider_path(service_provider)
 
-      expect(page).to have_content(strip_tags(I18n.t('service_provider_form.oidc_redirects')))
+      expect(page).to have_content(strip_tags(I18n.t('service_provider_form.oidc_redirects_html')))
     end
 
     scenario 'saml fields are shown on sp edit page when saml is selected' do
@@ -71,12 +71,11 @@ feature 'Service Providers CRUD' do
 
       visit edit_service_provider_path(service_provider)
 
-      expect(page).to have_content(strip_tags(I18n.t('service_provider_form.saml_redirects')))
+      expect(page).to have_content(strip_tags(I18n.t('service_provider_form.saml_redirects_html')))
       expect(page).to have_content(I18n.t('service_provider_form.saml_assertion_encryption'))
       # rubocop:disable Layout/LineLength
-      expect(page).to have_content(strip_tags(I18n.t('service_provider_form.assertion_consumer_service_url')))
-      expect(page).to have_content(strip_tags(I18n.t('service_provider_form.assertion_consumer_logout_service_url')))
-      expect(page).to have_content(strip_tags(I18n.t('service_provider_form.assertion_consumer_logout_service_url')))
+      expect(page).to have_content(strip_tags(I18n.t('service_provider_form.assertion_consumer_service_url_html')))
+      expect(page).to have_content(strip_tags(I18n.t('service_provider_form.assertion_consumer_logout_service_url_html')))
       # rubocop:enable Layout/LineLength
     end
 
@@ -87,7 +86,7 @@ feature 'Service Providers CRUD' do
 
       visit edit_service_provider_path(service_provider)
 
-      expect(page).to have_content(strip_tags(I18n.t('service_provider_form.oidc_redirects')))
+      expect(page).to have_content(strip_tags(I18n.t('service_provider_form.oidc_redirects_html')))
     end
 
     scenario 'can update service provider team', :js do
@@ -292,7 +291,7 @@ feature 'Service Providers CRUD' do
         expect(page).to_not have_content(t("simple_form.labels.service_provider.#{atr}"))
       end
 
-      expect(page).to have_content(strip_tags(t('service_provider_form.oidc_redirects')))
+      expect(page).to have_content(strip_tags(t('service_provider_form.oidc_redirects_html')))
     end
 
     scenario 'IAL1 attributes shown when IAL1 is selected', :js do
