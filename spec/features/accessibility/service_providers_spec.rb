107dca5e9ed7db18af3d47d15cf59823b7a24baf
davida marion <davida.marion@gsa.gov>
2024-04-02 12:53:15 -0400

    Add service provider view tests and fix broken html

--- spec/features/accessibility/service_providers_spec.rb
+++ spec/features/accessibility/service_providers_spec.rb
@@ -7,46 +7,70 @@ feature 'Service provider pages', :js do
   end
 
   context 'for admins' do
-    scenario 'all service providers page is accessible' do
-      admin = create(:admin)
-      login_as(admin)
+    let(:admin) { create(:admin) }
+    before { login_as(admin) }
 
+    scenario 'all service providers page is accessible' do
       visit service_providers_all_path
       expect_page_to_have_no_accessibility_violations(page)
+    end
+
+    context 'a service provider exists' do
+      let(:user) { create(:user, :with_teams) }
+      let(:app) { create(:service_provider, :with_users_team, user: user, logo: 'generic.svg') }
+
+      context 'show page' do
+        # admins have access to more features
+        scenario 'is accessible' do
+          visit service_provider_path(app)
+          expect_page_to_have_no_accessibility_violations(page)
+        end
+      end
+
+      context 'edit page' do
+        scenario 'is accessible' do
+          visit edit_service_provider_path(app)
+          expect_page_to_have_no_accessibility_violations(page)
+        end
+      end
     end
   end
 
   context 'for users' do
     let(:user) { create(:user, :with_teams) }
 
-    before do
-      login_as(user)
+    before { login_as(user) }
+
+    context 'index page' do
+      scenario 'accessible' do
+        visit service_providers_path
+        expect_page_to_have_no_accessibility_violations(page)
+      end
     end
 
-    scenario 'index page is accessible' do
-      visit service_providers_path
-      expect_page_to_have_no_accessibility_violations(page)
+    context 'new service provide page' do
+      scenario 'new service provider page' do
+        visit new_service_provider_path
+        expect_page_to_have_no_accessibility_violations(page)
+      end
     end
 
-    scenario 'new service provider page is accessible' do
-      visit new_service_provider_path
-      expect_page_to_have_no_accessibility_violations(page)
-    end
+    context 'service provider exists' do
+      let(:app) { create(:service_provider, :with_users_team, user: user, logo: 'generic.svg') }
 
-    scenario 'service provider details page is accessible' do
-      app = create(:service_provider, :with_users_team, user: user, logo: 'generic.svg')
-      login_as(user)
+      context 'show page' do
+        scenario 'is accessible' do
+          visit service_provider_path(app)
+          expect_page_to_have_no_accessibility_violations(page)
+        end
+      end
 
-      visit service_provider_path(app)
-      expect_page_to_have_no_accessibility_violations(page)
-    end
-
-    scenario 'service provider details edit page is accessible' do
-      app = create(:service_provider, :with_users_team, user: user)
-      login_as(user)
-
-      visit edit_service_provider_path(app)
-      expect_page_to_have_no_accessibility_violations(page)
+      context 'edit page' do
+        scenario 'is accessible' do
+          visit edit_service_provider_path(app)
+          expect_page_to_have_no_accessibility_violations(page)
+        end
+      end
     end
   end
 end
