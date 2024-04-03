79366ab99f401d70dc9d1b916928bc25ef787446
davida marion <davida.marion@gsa.gov>
2024-04-03 11:19:43 -0400

    Adds users acessibility tests

--- spec/features/accessibility/users_spec.rb
+++ spec/features/accessibility/users_spec.rb
@@ -2,24 +2,59 @@ require 'rails_helper'
 require 'axe-rspec'
 
 feature 'User pages', :js do
-  before do
-    admin = create(:admin)
-    login_as(admin)
+
+  context 'as an admin' do
+    let(:admin) { create(:admin) }
+    before { login_as(admin) }
+
+    context 'all_users view' do
+      scenario 'is accessible' do
+        visit users_path
+        expect_page_to_have_no_accessibility_violations(page)
+      end
+    end
+
+    context 'new_user view' do
+      scenario 'is accessible' do
+        visit new_user_path
+        expect_page_to_have_no_accessibility_violations(page)
+      end
+    end
+
+    context 'edit_user view' do
+      scenario 'is accessible' do
+        user = create(:user)
+        visit edit_user_path(user)
+        expect_page_to_have_no_accessibility_violations(page)
+      end
+    end
   end
 
-  scenario 'all users page is accessible' do
-    visit users_path
-    expect_page_to_have_no_accessibility_violations(page)
-  end
+  context 'as a non-admin' do
+    # these are unauthorized views, but we should ensure that error
+    # views pass accessibility tests
+    let(:user) { create(:user) }
+    before { login_as(user) }
 
-  scenario 'new user page is accessible' do
-    visit new_user_path
-    expect_page_to_have_no_accessibility_violations(page)
-  end
+    context 'all_users view' do
+      scenario 'is accessible' do
+        visit users_path
+        expect_page_to_have_no_accessibility_violations(page)
+      end
+    end
 
-  scenario 'edit user page is accessible' do
-    user = create(:user)
-    visit edit_user_path(user)
-    expect_page_to_have_no_accessibility_violations(page)
+    context 'new_user view' do
+      scenario 'is accessible' do
+        visit new_user_path
+        expect_page_to_have_no_accessibility_violations(page)
+      end
+    end
+
+    context 'edit_user view' do
+      scenario 'is accessible' do
+        visit edit_user_path(user)
+        expect_page_to_have_no_accessibility_violations(page)
+      end
+    end
   end
 end
