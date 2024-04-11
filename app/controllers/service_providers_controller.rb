446a8a75ac561389d471e94dd48f3afe0e0846e7
davida marion <davida.marion@gsa.gov>
2024-04-11 17:14:06 -0400

    Fix linting

--- app/controllers/service_providers_controller.rb
+++ app/controllers/service_providers_controller.rb
@@ -1,6 +1,7 @@
 class ServiceProvidersController < AuthenticatedController
   before_action -> { authorize ServiceProvider }, only: [:index, :create, :new, :all]
-  before_action -> { authorize service_provider, :member_or_admin? }, only: %i[update edit show destroy]
+  before_action -> {
+ authorize service_provider, :member_or_admin? }, only: %i[update edit show destroy]
   before_action :authorize_approval, only: [:update]
   before_action :authorize_allow_prompt_login, only: %i[create update]
   before_action :add_iaa_warning, except: %i[index destroy]
