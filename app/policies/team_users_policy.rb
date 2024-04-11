6be38862c73fef9ef3e26525cdaaeff48767f3a9
davida marion <davida.marion@gsa.gov>
2024-04-11 17:00:05 -0400

    Update TeamUsersPolicy and associated files

--- app/policies/team_users_policy.rb
+++ app/policies/team_users_policy.rb
@@ -1,38 +1,16 @@
 class TeamUsersPolicy < BasePolicy
-  attr_reader :current_user, :team
 
-  def initialize(current_user, team)
-    @current_user = current_user
-    @team = team
-  end
-
-  def index?
+  def manage_team_users?
     in_team? || admin?
-  end
-
-  def new?
-    in_team? || admin?
-  end
-
-  def create?
-    in_team? || admin?
-  end
-
-  def remove_confirm?
-    (admin? || in_team?) 
-  end
-
-  def destroy?
-    (admin? || in_team?) 
   end
 
   private
 
   def admin?
-    current_user&.admin?
+    user&.admin?
   end
 
   def in_team?
-    team.users.include?(current_user)
+    record.users.include?(user)
   end
 end
