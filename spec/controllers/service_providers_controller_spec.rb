7e7eee39af6cbb0ca697707f0d700463fefbb8d9
AJ Farkas <aj.farkas@gsa.gov>
2024-06-17 13:32:22 -0400

    LG-13432 work on tests

--- spec/controllers/service_providers_controller_spec.rb
+++ spec/controllers/service_providers_controller_spec.rb
@@ -1,23 +1,97 @@
 require 'rails_helper'
 
 describe ServiceProvidersController do
-  describe '#update' do
+  let(:user) { create(:user, :with_teams) }
+  let(:agency) { create(:agency, id: 123, name: 'GSA') }
+  let(:team) { create(:team, agency: agency) }
+  let(:init_help_params) do
+    { sign_in: {en: ''}, sign_up: {en: ''} , forgot_password: {en: ''} }
+  end
+  let(:sp) { create(:service_provider, :with_users_team,
+    user: user,
+    team: team,
+    agency_id: agency.id,
+    friendly_name: 'Acme Service',
+    help_text: init_help_params,
+  ) }
+  let(:fixture_path) { File.expand_path('../fixtures', __dir__) }
+  let(:logo_file_params) do
+    {
+      io: File.open(fixture_path + '/logo.svg'),
+      filename: 'alternative_filename.svg',
+      content_type: 'image/svg+xml',
+    }
+  end
+
+  describe '#create' do
     before do
       sign_in(user)
     end
 
-    let(:user) { create(:user, :with_teams) }
-    let(:sp) { create(:service_provider, :with_users_team, user: user) }
-    let(:fixture_path) { File.expand_path('../fixtures', __dir__) }
-    let(:init_help_params) do
-      { sign_in: {en: ''}, sign_up: {en: ''} , forgot_password: {en: ''} }
+    context 'help_text config' do
+
+      xit('should fill selected default options: blank set') do
+        help_params_0 = { sign_in: {en: 'blank'}, sign_up: {en: 'blank'} , forgot_password: {en: 'blank'} }
+        put :create, params: { id: sp.id, service_provider: {
+          issuer: sp.issuer,
+          friendly_name: sp.friendly_name,
+          agency: sp.agency,
+          agency_id: sp.agency_id,
+          help_text: help_params_0,
+        } }
+        sp.reload
+        expect(sp.help_text).to eq({
+          sign_in: {
+            en: "",
+            es: "",
+            fr: "",
+            zh: ""
+          },
+          sign_up: {
+            en: "",
+            es: "",
+            fr: "",
+            zh: ""
+          },
+          forgot_password: {
+            en: "",
+            es: "",
+            fr: "",
+            zh: ""
+          }
+        })
+      end
+      it('should fill selected default options: set 1') do
+        help_params_1 = { sign_in: {en: 'first_time'}, sign_up: {en: 'first_time'} , forgot_password: {en: 'troubleshoot_html'} }
+        put :create, params: { id: sp.id, service_provider: sp }
+        sp.reload
+        expect(sp.help_text).to eq({
+          sign_in: {
+            en: "First time here from Acme Service? Your old Acme Service username and password won’t work. Create a Login.gov account with the same email used previously.",
+            es: "¿Es la primera vez que visita Acme Service? Su antiguo nombre de usuario y contraseña de Acme Service ya no funcionan. Cree una cuenta en Login.gov con el mismo correo electrónico que usó anteriormente.", 
+            fr: "C’est la première fois que vous vous connectez à Acme Service? Vos anciens nom d’utilisateur et mot de passe pour accéder à Acme Service ne fonctionneront pas. Créez un compte Login.gov avec la même adresse e-mail que celle utilisée antérieurement.",
+            zh: "第一次从 Acme Service 来到这里？您的旧 Acme Service 用户名和密码将不起作用。用之前使用的同一电子邮件地址 来设立一个 Login.gov帐户。",
+          },
+          sign_up: {
+            en: "First time here from Acme Service? Your old Acme Service username and password won’t work. Create a Login.gov account with the same email used previously.",
+            es: "¿Es la primera vez que visita Acme Service? Su antiguo nombre de usuario y contraseña de Acme Service ya no funcionan. Cree una cuenta en Login.gov con el mismo correo electrónico que usó anteriormente.", 
+            fr: "C’est la première fois que vous vous connectez à Acme Service? Vos anciens nom d’utilisateur et mot de passe pour accéder à Acme Service ne fonctionneront pas. Créez un compte Login.gov avec la même adresse e-mail que celle utilisée antérieurement.",
+            zh: "第一次从 Acme Service 来到这里？您的旧 Acme Service 用户名和密码将不起作用。用之前使用的同一电子邮件地址 来设立一个 Login.gov帐户。",
+          },
+          forgot_password: {
+            en: "If you are having trouble accessing your Login.gov account, <a href='https://login.gov/help'>visit the Login.gov help center</a> for support.",
+            es: "Si tiene problemas para acceder a su cuenta de Login.gov, <a href='https://login.gov/es/help'>visite el centro de ayuda de Login.gov</a> para obtener asistencia.",
+            fr: "Si vous rencontrez des difficultés pour accéder à votre compte Login.gov, <a href='https://login.gov/fr/help'>veuillez vous rendre sur le site du centre d’assistance de Login.gov</a> pour obtenir de l’aide.",
+            zh: "如果您在访问 Login.gov 帐户时遇到问题，<a href='https://login.gov/zh/help'>请访问 Login.gov 帮助中心</a> 寻求支持。"
+          }
+        })
+      end
     end
-    let(:logo_file_params) do
-      {
-        io: File.open(fixture_path + '/logo.svg'),
-        filename: 'alternative_filename.svg',
-        content_type: 'image/svg+xml',
-      }
+  end
+
+  xdescribe '#update' do
+    before do
+      sign_in(user)
     end
 
     context 'when a user enters data into text inputs with leading and trailing spaces' do
@@ -171,7 +245,7 @@ describe ServiceProvidersController do
     end
   end
 
-  describe '#publish' do
+  xdescribe '#publish' do
     context 'no user' do
       it 'requires authentication' do
         post :publish
