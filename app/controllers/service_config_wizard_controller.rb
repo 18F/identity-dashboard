55a6135368e2ba4c99238654e18a9316a852f575
Howard M. Miller <howard.miller@gsa.gov>
2024-07-30 11:46:37 -0400

    Add logic for new config wizard flag

--- app/controllers/service_config_wizard_controller.rb
+++ app/controllers/service_config_wizard_controller.rb
@@ -3,6 +3,7 @@ class ServiceConfigWizardController < ApplicationController
   STEPS = %i[intro settings authentication issuer logo_and_cert redirects help_text]
   steps(*STEPS)
 
+  before_action :redirect_unless_flagged_in
   before_action -> { authorize step, policy_class: ServiceConfigPolicy }
   before_action :get_service_provider
   after_action :verify_authorized
@@ -25,5 +26,13 @@ class ServiceConfigWizardController < ApplicationController
 
   def get_service_provider
     @service_provider ||= policy_scope(ServiceProvider).new
+  end
+
+  def redirect_unless_flagged_in
+    unless IdentityConfig.store.service_config_wizard_enabled
+      redirect_to service_providers_path
+      return
+    end
+    redirect_to service_providers_path unless current_user.admin?
   end
 end
---
+++ spec/controllers/service_config_wizard_controller_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe ServiceConfigWizardController do
+  let(:user) { create(:user, uuid: SecureRandom.uuid, admin: false) }
+  let(:admin) { create(:user, uuid: SecureRandom.uuid, admin: true) }
+
+  def flag_in
+    expect(IdentityConfig.store).to receive(:service_config_wizard_enabled).and_return(true)
+  end
+
+  context 'as an admin' do
+    before do
+      sign_in admin
+    end
+
+    it 'can start the first step' do
+      flag_in
+      get :new
+      expect(response).to be_redirect
+      expect(response.redirect_url).to eq(service_config_wizard_url(Wicked::FIRST_STEP))
+    end
+
+    it 'will be redirected if the flag is not set' do
+      expect(IdentityConfig.store).to receive(:service_config_wizard_enabled).and_return(nil)
+      get :new
+      expect(response).to be_redirect
+      expect(response.redirect_url).to eq(service_providers_url)
+    end
+  end
+
+  context 'as a non-admin user' do
+    before do
+      sign_in user
+    end
+
+    it 'can start the first step' do
+      flag_in
+      get :new
+      expect(response).to be_redirect
+      expect(response.redirect_url).to eq(service_config_wizard_url(Wicked::FIRST_STEP))
+    end
+
+    it 'will be redirected if the flag is not set' do
+      expect(IdentityConfig.store).to receive(:service_config_wizard_enabled).and_return(nil)
+      get :new
+      expect(response).to be_redirect
+      expect(response.redirect_url).to eq(service_providers_url)
+    end
+  end
+end
