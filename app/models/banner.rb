0dcd8addd0f989339a109365122e808a99a5de3e
Jeremy Curcio <jeremy.curcio@gsa.gov>
2024-09-06 12:13:48 -0400

    Set times to start and end of day

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -1,20 +1,28 @@
 class Banner < ApplicationRecord
   validates :message, presence: true
-  validates :start_date, presence: { message: 'must be set if end date is set' }, if: :end_date?
+  # validates :start_date, presence: { message: 'must be set if end date is set' }, if: :end_date?
   validates :end_date, 
     comparison: { greater_than: :start_date, message: 'must be after start date' }, 
     if: :start_date?, 
     allow_blank: true
 
   def started?
-    start_date ? start_date < Time.zone.now : true
+    start_date ? start_time < Time.zone.now : true
   end
 
   def ended?
-    end_date && end_date < Time.zone.now
+    end_date && end_time < Time.zone.now
   end
 
   def active?
     started? && !ended?
+  end
+
+  def start_time
+    start_date&.beginning_of_day || created_at.beginning_of_day
+  end
+
+  def end_time
+    end_date.end_of_day
   end
 end
