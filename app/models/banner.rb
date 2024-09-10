61dd7c568cc567a79ce15adae70b758549dcad8e
Jeremy Curcio <jeremy.curcio@gsa.gov>
2024-09-10 07:54:41 -0400

    remove comment

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -1,6 +1,5 @@
 class Banner < ApplicationRecord
   validates :message, presence: true
-  # validates :start_date, presence: { message: 'must be set if end date is set' }, if: :end_date?
   validates :end_date, 
     comparison: { greater_than: :start_date, message: 'must be after start date' }, 
     if: :start_date?, 
