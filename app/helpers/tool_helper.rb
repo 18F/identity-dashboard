61d56482822cdf94ecae24a0f11c5062205c65a7
davida.marion <davida.marion@gsa.gov>
2024-09-13 13:59:58 +0000

    Adds xml syntax highlighting
    * remove extra newline
    
    * Add syntax highlighting to saml request validation tool
    
    * Makes rendered XML easier to read

--- app/helpers/tool_helper.rb
+++ app/helpers/tool_helper.rb
@@ -3,6 +3,12 @@ module ToolHelper
     tool_policy(sp).can_view_request_details?
   end
 
+  def syntax_highlight(xml)
+    formatter = Rouge::Formatters::HTML.new(css_class: 'highlight')
+    lexer = Rouge::Lexers::XML.new
+    formatter.format(lexer.lex(xml)).html_safe
+  end
+
   private
 
   def tool_policy(sp)
