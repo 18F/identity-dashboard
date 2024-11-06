a8d48846b82fb81a4878bf3ec120accc5a46d64b
Howard M. Miller <howard.miller@gsa.gov>
2024-11-06 11:11:04 -0500

    Pull user role list from the DB so we can verify migration + seeding worked

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -6,15 +6,17 @@ RSpec.describe Role, type: :model do
       expect do
         Role.seed
       end.to(change {Role.count}.by(4))
-      admin_roles = Role.where(name: 'Login Admin')
+      admin_roles = Role.where(name: 'Login.gov Admin')
       expect(admin_roles.count).to be(1)
     end
 
     it 'does not create a role that already exists' do
       existing_role = create(:role, name: 'Login.gov Admin')
       existing_attributes = existing_role.attributes
-      Role.seed
-      admin_roles = Role.where(name: 'Login Admin')
+      expect do
+        Role.seed
+      end.to(change {Role.count}.by(3))
+      admin_roles = Role.where(name: 'Login.gov Admin')
       expect(admin_roles.count).to be(1)
       expect(admin_roles.first.attributes).to eq(existing_attributes)
     end
