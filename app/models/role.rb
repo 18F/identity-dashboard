b976ec2afad89a71d9a134b03d3d8c4c9c629960
Howard M. Miller <howard.miller@gsa.gov>
2024-11-06 11:11:04 -0500

    Pull user role list from the DB so we can verify migration + seeding worked

--- app/models/role.rb
+++ app/models/role.rb
@@ -5,4 +5,17 @@ class Role < ApplicationRecord
     Role.find_or_create_by(name: 'Partner Developer')
     Role.find_or_create_by(name: 'Partner Readonly')
   end
+
+  # We should soon be able to remove this in favor of using Rails built-in methods
+  # such as `accepts_nested_attributes_for`
+  def self.radio_collection
+    all.each_with_object({}) do |role, collection|
+      collection[role.name] = role.legacy_admin?
+    end
+  end
+
+  def legacy_admin?
+    name == 'Login.gov Admin'
+  end
+
 end
