7c375500300d83bec9577c555bac3892b96825c1
Howard M. Miller <howard.miller@gsa.gov>
2024-11-20 12:22:03 -0500

    Rename to UserTeamPolicy to match model class & policy naming convention

--- spec/policies/user_team_policy_spec.rb
+++ spec/policies/user_team_policy_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe TeamUsersPolicy do
+describe UserTeamPolicy do
   let(:admin_user) { build(:admin) }
   let(:team_user) { build(:user) }
   let(:other_user) { build(:restricted_ic) }
@@ -12,15 +12,15 @@ describe TeamUsersPolicy do
 
   permissions :manage_team_users? do
     it 'allows team member to manage team users' do
-      expect(TeamUsersPolicy).to permit(team_user, team)
+      expect(UserTeamPolicy).to permit(team_user, team)
     end
 
     it 'allows admin to manage team users' do
-      expect(TeamUsersPolicy).to permit(team_user, team)
+      expect(UserTeamPolicy).to permit(team_user, team)
     end
 
     it 'does not allow a random user to manage team users' do
-      expect(TeamUsersPolicy).to_not permit(other_user, team)
+      expect(UserTeamPolicy).to_not permit(other_user, team)
     end
   end
 end
