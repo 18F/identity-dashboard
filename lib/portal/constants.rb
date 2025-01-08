3e2f133ff6f1e9f5dd8592fa584cfa17168b9850
AJ Farkas <aj.farkas@gsa.gov>
2025-01-08 12:19:43 -0500

    LG-15286 fix orphan menu + cleanup

--- lib/portal/constants.rb
+++ lib/portal/constants.rb
@@ -4,34 +4,34 @@ module Portal
   module Constants
     LANDING_CARDS = [
       {
-        :icon => 'groups',
-        :title => 'home.cards.rbac.title',
-        :body => 'home.cards.rbac.body',
+        icon: 'groups',
+        title: 'home.cards.rbac.title',
+        body: 'home.cards.rbac.body',
       },
       {
-        :icon => 'add_circle_outline',
-        :title => 'home.cards.new.title',
-        :body => 'home.cards.new.body',
+        icon: 'add_circle_outline',
+        title: 'home.cards.new.title',
+        body: 'home.cards.new.body',
       },
       {
-        :icon => 'edit',
-        :title => 'home.cards.edit.title',
-        :body => 'home.cards.edit.body',
+        icon: 'edit',
+        title: 'home.cards.edit.title',
+        body: 'home.cards.edit.body',
       },
       {
-        :icon => 'code',
-        :title => 'home.cards.test.title',
-        :body => 'home.cards.test.body',
+        icon: 'code',
+        title: 'home.cards.test.title',
+        body: 'home.cards.test.body',
       },
       {
-        :icon => 'search',
-        :title => 'home.cards.view.title',
-        :body => 'home.cards.view.body',
+        icon: 'search',
+        title: 'home.cards.view.title',
+        body: 'home.cards.view.body',
       },
       {
-        :icon => 'bug_report',
-        :title => 'home.cards.saml.title',
-        :body => 'home.cards.saml.body',
+        icon: 'bug_report',
+        title: 'home.cards.saml.title',
+        body: 'home.cards.saml.body',
       },
     ]
   end
