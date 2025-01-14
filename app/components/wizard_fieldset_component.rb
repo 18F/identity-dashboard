42dcdfeac88d1f4cbbef974e941ccec95e331862
howard.miller <howard.miller@gsa.gov>
2025-01-14 16:41:07 +0000

    More guided flow instruction updates
    * Update component tests to reduce false positives
    
    The component returns a Nokogiri object on render, which can transform strings with markup into a canonical representation and then throw off equality checks.
    
    Therefore, if we compare like with like (`to_s` on a Nokogiri object in both cases), that reduces inaccurate failures
    
    * Try to make quotes more consistent in service_providers/en.yml
    
    * Add `usa-link` to guided flow links
    
    * Guided flow updates based on MR feedback
    
    * Fix some guided flow typos
    
    * Add mising link to `bad_client_id` docs in guided flow
    
    * Rename partial locals to `describedby` where appropriate
    
    * Finish guided flow update text changes
    
    * Add ViewComponents for WizardFieldset
    
    * Update some of the guided flow text
    
    * Make "partner support help desk" case consistent
    
    See https://guides.18f.gov/content-guide/our-style/capitalization/
    
    > Overuse can cause users stress by implying formality or officialness where it doesn't exist.
    
    * Writing Style: Make "developer docs" capitalization consistent
    
    "the developer documentation", "our developer docs" — all lower-case
    "Login.gov Developer Docs" — always upper-case

---
+++ app/components/wizard_fieldset_component.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+# A fieldset for the ServiceConfigWizard
+class WizardFieldsetComponent < ViewComponent::Base
+  attr_reader :form, :input_type, :required, :model_method, :param_name, :description_key,
+              :inputs, :label_translation_key, :default
+
+  use_helper :accessible_label, from: ServiceConfigWizardHelper
+
+  INPUT_TYPES = %w[radio checkbox multi-text].freeze
+
+  # @param form [Method] such that form is a valid instance of wizard_form
+  # @param input_type [String] must be one of the values of INPUT_TYPES
+  # @param param_name [Symbol] the parameter name to use when submitting the form
+  # @param inputs [Hash] input items such that the input fields are rendered correctly
+  # @param options [Hash] additional options:
+  #
+  # @option options [String] :description_key a translation subkey under `service_provider_form`
+  #   defaults to `"#{model_method}_html"`, pass an empty string to skip the description
+  # @option options [Boolean] :required whether this field is required
+  # @option options [Symbol,String] :model_method the method to use when pulling the current value,
+  #           defaults to `param_name`
+  # @option options [Symbol,String] :label_key if the label translation for the field should be
+  #           different from `model_method`
+  # @option options [String] :default to pre-select an option
+  def initialize(form:, input_type:, param_name:, inputs: nil, **options) # rubocop:disable Lint/MissingSuper
+    unless INPUT_TYPES.include? input_type
+      raise ArgumentError, "#{self.class}: invalid `input_type` '#{input_type}"
+    end
+
+    @form = form
+    @input_type = input_type
+    @param_name = param_name
+    @inputs = inputs
+    @model_method = options[:model_method] || @param_name
+    @required = options[:required]
+    @description_key = options[:description_key] || "#{model_method}_html"
+    @label_translation_key = options[:label_key] || model_method
+    @default = options[:default]
+  end
+
+  def required_class
+    required ? 'required' : 'optional'
+  end
+
+  def text_list
+    inputs.blank? ? [''] : Array(inputs)
+  end
+
+  def description?
+    @description_key.present?
+  end
+
+  def html_key
+    @html_key ||= label_translation_key.parameterize
+  end
+end
