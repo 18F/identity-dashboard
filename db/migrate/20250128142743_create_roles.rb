113f4feb7df1a177e76630107e3a23db9225d3fb
AJ Farkas <aj.farkas@gsa.gov>
2025-01-28 09:31:02 -0500

    LG-15285 create roles migration

--- db/migrate/20250128142743_create_roles.rb
+++ db/migrate/20250128142743_create_roles.rb
@@ -2,10 +2,11 @@ class CreateRoles < ActiveRecord::Migration[7.1]
   def change
     create_table :roles do |t|
       t.string :name, null: false
+      t.string :friendly_name, null: false
 
       t.timestamps
 
-      t.index ['name'], name: 'index_roles_on_name', unique: true
+      t.index :name
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_06_175024) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_28_142743) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -82,11 +82,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_06_175024) do
   end
 
   create_table "roles", force: :cascade do |t|
-    t.datetime "created_at", precision: nil
-    t.datetime "updated_at", precision: nil
     t.string "name"
     t.string "friendly_name"
-    t.index ["name"], name: 'index_roles_on_name', unique: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_roles_on_name"
   end
 
   create_table "security_events", force: :cascade do |t|
