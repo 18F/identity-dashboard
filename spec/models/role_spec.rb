2bf4a8179be18f6b85cf269e0421e44ef7d7e113
Howard M. Miller <howard.miller@gsa.gov>
2025-02-04 14:05:14 -0500

    Fix CI setup problem with initializer

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -8,4 +8,16 @@ RSpec.describe Role, type: :model do
       expect(Role.find_by(name: other_role)).not_to be_legacy_admin
     end
   end
+
+  it 'initializes roles' do
+    logger = object_double(Rails.logger)
+    allow(logger).to receive(:info).with(any_args)
+
+    Role.find_each(&:destroy)
+    Role.initialize_roles { |message| logger.info message }
+    expect(logger).to have_received(:info).with 'logingov_admin added to roles as Login.gov Admin'
+    expect(logger).to have_received(:info).with 'partner_admin added to roles as Partner Admin'
+    expect(logger).to have_received(:info).with 'partner_developer added to roles as Partner Developer'
+    expect(logger).to have_received(:info).with 'partner_readonly added to roles as Partner Readonly'
+  end
 end
