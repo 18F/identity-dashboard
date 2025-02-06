fa0cb3c632ae041b4ee5caa97438d513acd16f7f
Jeremy Curcio <jeremy.curcio@gsa.gov>
2025-02-06 12:01:35 -0500

    Move login admin and partner admin checks to base policy

--- app/policies/team_policy.rb
+++ app/policies/team_policy.rb
@@ -40,20 +40,4 @@ class TeamPolicy < BasePolicy
   def in_team?
     record.users.include?(user)
   end
-
-  def user_has_login_admin_role?
-    return admin? unless IdentityConfig.store.access_controls_enabled
-
-    admin? || user.user_teams.any? do |membership|
-      membership.role == Role.find_by(name: 'Login.gov Admin')
-    end
-  end
-
-  def user_has_partner_admin_role?
-    return true unless IdentityConfig.store.access_controls_enabled
-
-    user.user_teams.any? do |membership|
-      membership.role == Role.find_by(name: 'Partner Admin')
-    end
-  end
 end
