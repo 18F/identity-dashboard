57ddec3cdd9466dc0eadea6e23ca37a044d187f3
Nadya Primak <nadya.primak@gsa.gov>
2025-02-11 16:23:21 -0500

    fix rubocop lint errors

--- app/services/populate_roles.rb
+++ app/services/populate_roles.rb
@@ -7,18 +7,21 @@ class PopulateRoles
       WARNING: this will loop through all UserTeams with invalid or nil roles and reset roles based on legacy permissions
     WARN
 
-    VALID_ROLENAMES = ["partner_admin", "partner_developer", "partner_readonly", "logingov_admin"]
-  
+    VALID_ROLENAMES = %w[partner_admin partner_developer partner_readonly logingov_admin]
+
     def initialize(logger)
         @logger = logger
         @logger.warn(USAGE_WARNING)
-        @userteamswithoutrole = UserTeam.where(role_name: nil).or UserTeam.where.not(role_name: PopulateRoles::VALID_ROLENAMES)
+        @userteamswithoutrole = UserTeam.where(role_name: nil)
+          .or UserTeam.where.not(role_name: PopulateRoles::VALID_ROLENAMES)
         # check against array of exact role names (not friendly names)
         @logger.info(@userteamswithoutrole)
     end
-  
+
     def call
-      @logger.info("INFO: All UserTeams already have valid roles.") if @userteamswithoutrole.length == 0
+      if @userteamswithoutrole.length == 0
+        @logger.info('INFO: All UserTeams already have valid roles.')
+      end
       begin
         @userteamswithoutrole.each do |userteam|
           user = get_user(userteam)
@@ -29,19 +32,20 @@ class PopulateRoles
         rescue StandardError => err
           @logger.warn("ERROR: #{err}")
       end
-      @logger.info("SUCCESS: All invalid UserTeams have been updated")
+      @logger.info('SUCCESS: All invalid UserTeams have been updated')
     end
-  
+
     private
-  
+
     def get_user(userteam)
         User.find(userteam.user_id)
     end
 
     def get_legacy_role(user)
-        #partner_admin = legacy allowlisted
+        # partner_admin = legacy allowlisted
         return 'partner_admin' if allowlisted_user?(user)
-        return 'partner_developer'
+
+        'partner_developer'
     end
 
     def set_role(userteam, role)
@@ -50,4 +54,3 @@ class PopulateRoles
     end
 
   end
-  
--- spec/services/populate_roles_spec.rb
+++ spec/services/populate_roles_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
 describe PopulateRoles do
-  let(:gov_account)  { 'test@example.gov,Robert,Smith' }
+  let(:gov_account) { 'test@example.gov,Robert,Smith' }
   let(:gov_email)      { gov_account.split(',')[0] }
   let(:gov_first_name) { gov_account.split(',')[1] }
   let(:gov_last_name)  { gov_account.split(',')[2] }
 
-  let(:nongov_account)  { 'test@example.com,Bob,Walters' }
+  let(:nongov_account) { 'test@example.com,Bob,Walters' }
   let(:nongov_email)      { nongov_account.split(',')[0] }
   let(:nongov_first_name) { nongov_account.split(',')[1] }
   let(:nongov_last_name)  { nongov_account.split(',')[2] }
 
   let(:without_role_membership) { create(:user_team) }
   let(:with_role_membership) { create(:user_team, :partner_developer) }
-  let(:logger) {instance_double(Logger)}
+  let(:logger) { instance_double(Logger) }
 
   subject { described_class.new(logger) }
 
@@ -34,8 +34,9 @@ describe PopulateRoles do
         user.user_teams << without_role_membership
         subject.call
         user.reload
-        expect(user.user_teams.first.role_name).to eq("partner_admin")
-        expect(logger).to have_received(:info).with("SUCCESS: All invalid UserTeams have been updated")
+        expect(user.user_teams.first.role_name).to eq('partner_admin')
+        expect(logger).to have_received(:info)
+          .with('SUCCESS: All invalid UserTeams have been updated')
       end
     end
 
@@ -50,8 +51,9 @@ describe PopulateRoles do
         user.user_teams << without_role_membership
         subject.call
         user.reload
-        expect(user.user_teams.first.role_name).to eq("partner_developer")
-        expect(logger).to have_received(:info).with("SUCCESS: All invalid UserTeams have been updated")
+        expect(user.user_teams.first.role_name).to eq('partner_developer')
+        expect(logger).to have_received(:info)
+          .with('SUCCESS: All invalid UserTeams have been updated')
       end
     end
 
@@ -65,7 +67,8 @@ describe PopulateRoles do
             )
             user.user_teams << with_role_membership
             subject.call
-            expect(logger).to have_received(:info).with("INFO: All UserTeams already have valid roles.")
+            expect(logger).to have_received(:info)
+              .with('INFO: All UserTeams already have valid roles.')
         end
     end
   end
