d6991d58969e49879b665e90c8f8f4cf18f8daab
Howard M. Miller <howard.miller@gsa.gov>
2025-02-14 16:30:10 -0500

    Add new ServiceProvider status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_28_142743) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_14_203312) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -136,6 +136,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_28_142743) do
     t.string "app_name", default: "", null: false
     t.boolean "prod_config", default: false, null: false
     t.string "post_idv_follow_up_url"
+    t.string "status", default: "pending"
     t.index ["group_id"], name: "index_service_providers_on_group_id"
     t.index ["issuer"], name: "index_service_providers_on_issuer", unique: true
   end
