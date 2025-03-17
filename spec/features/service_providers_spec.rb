906a570c6a0598a42539984e5d191f9ab2be1f18
Nadya Primak <nadya.primak@gsa.gov>
2025-03-17 16:09:25 -0400

    remove duplicate test

--- spec/features/service_providers_spec.rb
+++ spec/features/service_providers_spec.rb
@@ -547,17 +547,6 @@ feature 'Service Providers CRUD' do
       expect(page).to have_content(sp.issuer)
     end
 
-    scenario 'can see and visit link to analytics path' do
-      user_team = create(:user_team, :logingov_admin, user: user_to_log_in_as)
-      sp = create(:service_provider, team: user_team.team)
-      visit service_providers_path
-      data_link = sp.friendly_name + ' data'
-      expect(page).to have_content(data_link)
-      click_on data_link
-      expect(page).to have_content('App Analytics Dashboard')
-      expect(page).to have_content(sp.issuer)
-    end
-
     scenario 'can enable prompt=login for a service provider' do
       user_to_log_in_as = logingov_admin
       sp = create(:service_provider, :with_team)
