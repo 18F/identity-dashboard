52c77d7ed77042146097b890226eed95c0dccc2c
Colter <59977618+colter-nattrass@users.noreply.github.com>
2025-03-28 15:12:08 -0600

    addin appropriate hardcoded role arn and quicksight client session details

--- app/controllers/analytics/service_providers_controller.rb
+++ app/controllers/analytics/service_providers_controller.rb
@@ -9,13 +9,14 @@ class Analytics::ServiceProvidersController < ApplicationController
     DASHBOARD_ID = 'ee5562fd-c6e9-4e5d-a234-1875ed36379a'
     EMAIL = 'admin@gsa.gov'
     REGION = 'us-west-2'
-    ROLE_ARN = 'arn:aws:iam::487317109730:role/'
+    ROLE_ARN = 'arn:aws:iam::894947205914:role/cnattrass_portal_iam_role'
     ROLE_SESSION_NAME = 'YourSessionName'
 
 
     def show
         @issuer = service_provider.issuer
         @friendly_name = service_provider.friendly_name.capitalize
+
         @quicksight_user = register_quicksight_user(quicksight_client, AWS_ACCOUNT_ID, EMAIL,
 REGION)
         @dashboard_embed_url = fetch_quicksight_embed_url_for_registered_user(quicksight_client, AWS_ACCOUNT_ID, USER_ARN, DASHBOARD_ID, REGION) # rubocop:disable Layout/LineLength
@@ -49,7 +50,15 @@ logo_file_attachment: :blob).find(id)
     end
 
     def quicksight_client
-        @quicksight_client ||= Aws::QuickSight::Client.new(region: REGION)
+        @quicksight_client ||= begin
+            assumed_role = assume_role(sts_client, ROLE_ARN, ROLE_SESSION_NAME)
+            credentials = Aws::Credentials.new(
+                assumed_role.credentials.access_key_id,
+                assumed_role.credentials.secret_access_key,
+                assumed_role.credentials.session_token,
+            )
+            Aws::QuickSight::Client.new(region: REGION, credentials: credentials)
+        end
     end
 
     def register_quicksight_user(client, aws_account_id, email, region)
