d2f1238d9b205384f15d5e6549c585f3bd1e455e
Jeremy Curcio <jeremy.curcio@gsa.gov>
2025-04-07 11:26:42 -0400

    Reoder the constants to be in alphabetical order

--- app/models/zendesk_request.rb
+++ app/models/zendesk_request.rb
@@ -5,13 +5,13 @@ class ZendeskRequest
   ZENDESK_TICKET_FORM_ID = 5663417357332
 
   ZENDESK_TICKET_FIELD_FUNCTIONS = {
-    20697165967508 => -> (record) { record.logo.present? }, # Logo attestation
     4418412738836 => -> (record) { record.agency.name.parameterize.underscore }, # Agency
-    23180053076628 => -> (record) { record.issuer }, # Issuer
     4417492827796 => -> (record) { record.app_name }, # Application Name
     5064895580308 => -> (record) { record.description }, # Ticket Description
-    4418367585684 => -> (record) { 'on' }, # Ready to move to production attestation
+    23180053076628 => -> (record) { record.issuer }, # Issuer
+    20697165967508 => -> (record) { record.logo.present? }, # Logo attestation
     4417169610388 => -> (record) { 'new_integration' }, # Request type
+    4418367585684 => -> (record) { 'on' }, # Ready to move to production attestation
   }
 
   # This is separete because the host isn't available in the model
@@ -19,9 +19,6 @@ class ZendeskRequest
   ZENDESK_IAL_VALUE_ID = 4417494977300
 
   ZENDESK_TICKET_FIELD_INFORMATION = {
-    4417546214292 => { label: 'iaa_number',
-      placeholder: 'LGABCFY210001-0001-0000',
-      input_type: 'text' },
     4417940288916 => { label: 'application_url',
       placeholder: 'https://yourapp.gov/',
       input_type: 'text' },
@@ -37,32 +34,35 @@ class ZendeskRequest
         { label: 'Other', value: 'other' },
       ],
     },
-    4417514509076 => { label: 'est_annual_reg',
-      placeholder: 100000,
-      input_type: 'number' },
-    14323273767572 => { label: 'monthly_active_users',
-      placeholder: 20000,
-      input_type: 'number' },
-    14326923502100 => { label: 'seasonality',
-      placeholder: nil,
-      input_type: 'text' },
-    4417513940756 => { label: 'est_auths',
-      placeholder: 50000,
-      input_type: 'number' },
-    4417512374548 => { label: 'launch_date',
-      placeholder: nil,
-      input_type: 'date' },
     4417547364628 => { label: 'billing_pocs',
       placeholder: 'First Last - Director, Department - first.last@agency.gov - (555) 555-5555',
       input_type: 'text' },
+    4417514509076 => { label: 'est_annual_reg',
+      placeholder: 100000,
+      input_type: 'number' },
+    4417513940756 => { label: 'est_auths',
+      placeholder: 50000,
+      input_type: 'number' },
+    4975909708564 => { label: 'help_desk_contact_info',
+      placeholder: '(555) 555-5555 - support@agency.gov',
+      input_type: 'text' },
+    4417546214292 => { label: 'iaa_number',
+      placeholder: 'LGABCFY210001-0001-0000',
+      input_type: 'text' },
+    4417512374548 => { label: 'launch_date',
+      placeholder: nil,
+      input_type: 'date' },
+    14323273767572 => { label: 'monthly_active_users',
+      placeholder: 20000,
+      input_type: 'number' },
     4417948190868 => { label: 'pm_pocs',
       placeholder: 'First Last - Program Manager - first.last@agency.gov - (555) 555-5555',
       input_type: 'text' },
+    14326923502100 => { label: 'seasonality',
+      placeholder: nil,
+      input_type: 'text' },
     4417940248340 => { label: 'tech_support_pocs',
       placeholder: 'First Last - Technical Lead - first.last@agency.gov - (555) 555-5555',
-      input_type: 'text' },
-    4975909708564 => { label: 'help_desk_contact_info',
-      placeholder: '(555) 555-5555 - support@agency.gov',
       input_type: 'text' },
   }
 
