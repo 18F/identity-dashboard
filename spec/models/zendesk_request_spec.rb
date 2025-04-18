d3b1f6e9ef696fe26e7ee4c0777556608aacea77
AJ Farkas <aj.farkas@gsa.gov>
2025-04-18 10:49:08 -0400

    LG-15848 add tests

--- spec/models/zendesk_request_spec.rb
+++ spec/models/zendesk_request_spec.rb
@@ -12,6 +12,28 @@ describe ZendeskRequest do
   let(:custom_fields) { [{ "id":4418367585684, "value":'on' }, { "id":20697165967508, "value":'on' }, { "id":4418412738836, "value":'General Services Administration' }, { "id":4417546214292, "value":'LGMIA999999' }, { "id":4417547364628, "value":'BillingPOC TestUser - test.user@gsa.gov - 555-555-1234' }, { "id":4417948129556, "value":'https://portal.int.identitysandbox.gov/service_providers/9999' }, { "id":23180053076628, "value":'urn:issuer:testing:gsa:test_application' }, { "id":4417940288916, "value":'https://fakeapplication.gov/logingov' }, { "id":4417492827796, "value":'Application Name - Testing Application' }, { "id":5064895580308, "value":'Application Description' }, { "id":14323206118676, "value":'General public' }, { "id":4417514509076, "value":'100000' }, { "id":14323273767572, "value":'1000' }, { "id":14326923502100, "value":'All Year Seasonality' }, { "id":4417513940756, "value":'1200000' }, { "id":4417494977300, "value":'ial1' }, { "id":4417512374548, "value":'2025-01-01' }, { "id":4417948190868, "value":'PM - test.user@gsa.gov - 555-555-1234' }, { "id":4417940248340, "value":'Techsupport - test.user@gsa.gov - 555-555-1234' }, { "id":4975909708564, "value":'Helpdesk Contact Info' }, { "id":4417169610388, "value":'new_integration' }] }
   # rubocop:enable Layout/LineLength
 
+  describe 'ticket field functions' do
+    context 'in prod_like_env' do
+      before do
+        allow(IdentityConfig.store).to receive_messages(prod_like_env: true)
+      end
+
+      it 'sets the request type to integration_change' do
+        expect(zendesk_request.ticket_field_functions[4417169610388].call(sp)).to eq('integration_change')
+      end
+    end
+
+    context 'in sandbox env' do
+      before do
+        allow(IdentityConfig.store).to receive_messages(prod_like_env: false)
+      end
+
+      it 'sets the request type to new_integration' do
+        expect(zendesk_request.ticket_field_functions[4417169610388].call(sp)).to eq('new_integration')
+      end
+    end
+  end
+
   describe 'initialize' do
     it 'initializes' do
       expect(zendesk_request.requestor).to eq(user)
