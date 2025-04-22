692532cb62fa4a23498db5e2bd4430da543b2787
Howard M. Miller <howard.miller@gsa.gov>
2025-04-22 15:36:09 -0400

    Refactor textToClipboard JS so it works again while passing linter

--- app/javascript/packs/text_to_clipboard.js
+++ app/javascript/packs/text_to_clipboard.js
@@ -1,25 +1,23 @@
-class TextToClipboard {
-  constructor(element) {
-    const messagePlaceholder = element.getElementsByClassName('clipboard-message-placeholder')[0];
-    const [button] = element.getElementsByTagName('button');
-    [this.input] = element.getElementsByClassName('clipboard-input');
-    if (!(messagePlaceholder && this.input && button)) { return; }
+function textToClipboard(element) {
+  const messagePlaceholder = element.getElementsByClassName('clipboard-message-placeholder')[0];
+  const [button] = element.getElementsByTagName('button');
+  const [input] = element.getElementsByClassName('clipboard-input');
+  if (!(messagePlaceholder && input && button)) { return; }
 
-    this.message = document.createElement("span");
-    this.message.innerHTML = "Copied to clipboard!";
-    messagePlaceholder.append(this.message);
-    button.addEventListener('click', (_e) => {
-      navigator.clipboard.writeText(this.input.value);
-      this.message.classList.add('is-visible');
-      setTimeout(() => {
-        this.message.classList.remove('is-visible');
-      }, 2000);
-    });
-  }
+  const message = document.createElement("span");
+  message.innerHTML = "Copied to clipboard!";
+  messagePlaceholder.append(this.message);
+  button.addEventListener('click', (_e) => {
+    navigator.clipboard.writeText(input.value);
+    message.classList.add('is-visible');
+    setTimeout(() => {
+      message.classList.remove('is-visible');
+    }, 2000);
+  });
 }
 
 window.addEventListener('DOMContentLoaded', () => {
   for (const item of document.getElementsByClassName('text-to-clipboard-wrapper')) {
-    TextToClipboard(item);
+    textToClipboard(item);
   }
 });
