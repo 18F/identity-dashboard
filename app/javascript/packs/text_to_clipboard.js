b9333cdd10e14b101a0762e7fe93149cb578d259
Howard M. Miller <howard.miller@gsa.gov>
2025-04-23 17:18:22 -0400

    Update auth_token clipboard JS per code review feedback

--- app/javascript/packs/text_to_clipboard.js
+++ app/javascript/packs/text_to_clipboard.js
@@ -1,17 +1,17 @@
 function textToClipboard(element) {
-  const messagePlaceholder = element.getElementsByClassName('clipboard-message-placeholder')[0];
-  const [button] = element.getElementsByTagName('button');
-  const [input] = element.getElementsByClassName('clipboard-input');
-  if (!(messagePlaceholder && input && button)) { return; }
+  const message = element.querySelector('.clipboard-message');
+  const button = element.querySelector('button');
+  const input = element.querySelector('.clipboard-input');
+  if (!(message && input && button)) { return; }
 
-  const message = document.createElement("span");
-  message.innerHTML = "Copied to clipboard!";
-  messagePlaceholder.append(message);
   button.addEventListener('click', (_e) => {
     navigator.clipboard.writeText(input.value);
-    message.classList.add('is-visible');
+    message.innerHTML = 'Copied to clipboard!';
+    message.classList.add('opacity-100');
+    message.classList.remove('opacity-0');
     setTimeout(() => {
-      message.classList.remove('is-visible');
+      message.classList.add('opacity-0');
+      message.classList.remove('opacity-100');
     }, 2000);
   });
 }
