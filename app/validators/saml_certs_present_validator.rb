e851a35407eec3aba10dc3b037b814c84a1e159c
Jeremy Curcio <jeremy.curcio@gsa.gov>
2025-05-15 08:54:37 -0400

    Remove extraneous white space

--- app/validators/saml_certs_present_validator.rb
+++ app/validators/saml_certs_present_validator.rb
@@ -4,7 +4,5 @@ class SamlCertsPresentValidator < ActiveModel::Validator
       return unless record.certs.blank?
 
         record.errors.add(:certs, I18n.t('service_provider_form.errors.certs.saml_no_cert'))
-
-
   end
 end
