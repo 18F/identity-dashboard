11246f10e352b554bb8afbf48a82f2916ce18c98
AJ Farkas <aj.farkas@gsa.gov>
2025-05-23 16:40:59 -0400

    Portal#117 test home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,14 +5,18 @@ class HomeController < ApplicationController
     @canonical_url = (!user_signed_in?) ? request.base_url.gsub('portal', 'dashboard') : nil
     render :index and return unless user_signed_in?
 
-    includes = %i[users service_providers agency]
-    @teams = current_user.teams.includes(*includes).all
-
-    render 'home/authenticated/index'
+    signed_in_redirect
   end
 
   def system_use
     render 'home/system_use' and return unless user_signed_in?
+
+    signed_in_redirect
+  end
+
+  def signed_in_redirect
+    includes = %i[users service_providers agency]
+    @teams = current_user.teams.includes(*includes).all
 
     render 'home/authenticated/index'
   end
