21892e8121b9e2d45368489ec28cb89787ae0cba
Jeremy Curcio <jeremy.curcio@gsa.gov>
2025-05-29 14:54:30 -0400

    Revert this back, almost

--- app/models/zendesk_request.rb
+++ app/models/zendesk_request.rb
@@ -130,26 +130,23 @@ class ZendeskRequest
 
     @conn ||= Faraday.new(url: ZENDESK_BASE_URL, headers: headers)
 
-    # resp = @conn.post(ZENDESK_POST_PATH) { |req| req.body = ticket_data.to_json }
-    # response = JSON.parse(resp.body)
+    resp = @conn.post(ZENDESK_POST_PATH) { |req| req.body = ticket_data.to_json }
+    response = JSON.parse(resp.body)
 
-    response_info = {request: ticket_data.to_json}
-
-    response_info
-    # if resp.status == 201
-    #   ticket_id = response.dig('request', 'id')
-    #   { success: true, ticket_id: ticket_id }
-    # else
-    #   errors = response.dig('details', 'base')
-    #   if (errors)
-    #     parsed_errors = []
-    #     errors.each do |e|
-    #       parsed_errors.push(e['description'])
-    #     end
-    #     return { success: false, errors: parsed_errors }
-    #   end
-    #   { success: false, errors: [] }
-    # end
+    if resp.status == 201
+      ticket_id = response.dig('request', 'id')
+      { success: true, ticket_id: ticket_id }
+    else
+      errors = response.dig('details', 'base')
+      if (errors)
+        parsed_errors = []
+        errors.each do |e|
+          parsed_errors.push(e['description'])
+        end
+        return { success: false, errors: parsed_errors }
+      end
+      { success: false, errors: [] }
+    end
 
   end
 end
