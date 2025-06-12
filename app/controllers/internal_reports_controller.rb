670ba0ab3e0e023cc7d62d35008b6295325933c3
Howard M. Miller <howard.miller@gsa.gov>
2025-06-12 13:31:19 -0400

    Use more reliable CSV generation than having an ERB template

--- app/controllers/internal_reports_controller.rb
+++ app/controllers/internal_reports_controller.rb
@@ -2,9 +2,11 @@ class InternalReportsController < AuthenticatedController
   before_action :admin_only
 
   def memberships
-    @memberships = UserTeam.left_joins(:user, :role)
+    memberships = UserTeam.left_joins(:user, :role)
       .select(:id, :user_id, :group_id, :role_name, roles: [:friendly_name])
       .order('users.email', 'roles.id', :group_id)
+
+    render renderable: MembershipCsv.new(memberships)
   end
 
   private
