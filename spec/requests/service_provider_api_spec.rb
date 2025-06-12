3c5451aa6a1bd2d7950cf3d42abea3f9b3e87c7b
Jeremy Curcio <jeremy.curcio@gsa.gov>
2025-06-12 11:04:11 -0400

    Fix failing tests

--- spec/requests/service_provider_api_spec.rb
+++ spec/requests/service_provider_api_spec.rb
@@ -1,26 +1,12 @@
 require 'rails_helper'
 
 describe 'Service Provider API' do
-  it 'fails without authorization header' do
-    app = create(:service_provider)
-    get api_service_providers_path
-    json = response.parsed_body
-    # expect(response).to have_http_status(:unauthorized)
-  end
-
-  context 'with API token' do
-    let(:auth_header) do
-      token = "alskdjf;lksajdflk;jsd#{rand(1..1000)}"
-      email = 'no-reply@gsa.gov'
-      { 'Authorization' => "Token #{token}; email=#{email}" }
-    end
-
-    it 'returns JSON' do
+  context 'without API token' do
+    it 'fails without authorization header' do
       app = create(:service_provider)
-      get api_service_providers_path, headers: auth_header # wrong array length at 1 (expected 2, was 1)
+      get api_service_providers_path
       json = response.parsed_body
-      expect(response).to have_http_status(:ok)
-      expect(json[0]['issuer']).to eq(app.issuer)
+      expect(response).to have_http_status(:unauthorized)
     end
   end
 
@@ -33,17 +19,16 @@ describe 'Service Provider API' do
     end
 
     let(:auth_header) do
-      user = build(:user)
-      credentials = { token: token, email: user.email}
-      { 'Authorization' => credentials }
+      { 'Authorization' => "Token #{token}, email=#{user.email}" }
     end
 
     it 'returns JSON' do
       app = create(:service_provider)
-      get api_service_providers_path, headers: auth_header # undefined method 'sub' for an instance of Hash
+      get api_service_providers_path, headers: auth_header
       json = response.parsed_body
+      puts request.inspect
       expect(response).to have_http_status(:ok)
       expect(json[0]['issuer']).to eq(app.issuer)
-   end
+    end
   end
 end
