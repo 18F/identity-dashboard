701211569e231adb9a2d2ae6bc24c455eaa06ebe
gerardo.cruz-ortiz <gerardo.cruz-ortiz@gsa.gov>
2025-07-22 12:40:05 -0400

    JS Linter Fixes

--- spec/controllers/analytics/service_providers_controller_spec.rb
+++ spec/controllers/analytics/service_providers_controller_spec.rb
@@ -36,7 +36,8 @@ describe Analytics::ServiceProvidersController do
           let(:id) { 'not-real-sp' }
 
           it 'returns status not found' do
-            # this currently fails because invalid ids return unauthorized instead of not found. This matches the pattern in the base service_providers controller
+            # this currently fails because invalid ids return unauthorized instead of not found.
+            # This matches the pattern in the base service_providers controller
             get :show, params: { id: }
             expect(response).to have_http_status(:not_found)
           end
@@ -63,6 +64,7 @@ describe Analytics::ServiceProvidersController do
        get :stream_daily_auths_report,
         params: { id:, date:, year: }
      end
+
      # this is failing because when users are not logged in the portal redirects to the homepage
      context 'when a user does not exist' do
        it 'returns unauthorized' do
