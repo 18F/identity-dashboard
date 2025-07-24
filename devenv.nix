7af48e7e1d2c9b5c47a2c191bd137eac73b25c98
Henry Richardson <henry.richardson@gsa.gov>
2025-07-24 12:19:54 -0400

    feat: add initial devenv configuration
    - Added `.gitignore` entries for development environment files, direnv, and pre-commit configuration.
    - Introduced `.gitlab-ci-devenv.yml` for GitLab CI integration with Nix-based development environment.
    - Added `.gitlab-ci-rules.yml` to define reusable CI rules.
    - Updated `.gitlab-ci.yml` to include new CI configuration files.
    - Added `devenv.lock` to lock dependencies for the Nix-based development environment.
    - Created `devenv.nix` to define the development environment with Ruby, JavaScript, PostgreSQL, and Redis support.
    - Added `devenv.yaml` to specify Nix inputs for the development environment.
    
    This commit sets up a reproducible development environment and integrates it with GitLab CI for streamlined workflows.

---
+++ devenv.nix
@@ -0,0 +1,55 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+
+let
+  pkgs-unstable = import inputs.nixpkgs-unstable { system = pkgs.stdenv.system; };
+in
+{
+
+  packages = with pkgs; [
+    pkgs-unstable.chromedriver
+    git
+    libyaml
+  ];
+
+  languages = {
+    ruby = {
+      enable = true;
+      bundler.enable = true;
+      versionFile = ./.ruby-version;
+    };
+    javascript = {
+      enable = true;
+      yarn.enable = true;
+    };
+  };
+
+  enterShell = ''
+    # Conflicts with bundler
+    export RUBYLIB=
+  '';
+
+  tasks = {
+    "ruby:install_gems" = {
+      exec = "bundle install";
+      status = "bundle check";
+      before = [ "devenv:enterShell" ];
+    };
+  };
+
+  services = {
+    postgres = {
+      enable = true;
+      package = pkgs.postgresql_16;
+      listen_addresses = "127.0.0.1";
+    };
+    redis = {
+      enable = true;
+    };
+  };
+}
