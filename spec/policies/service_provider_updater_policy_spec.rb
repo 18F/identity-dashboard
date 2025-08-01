be0e4b86104630129d58fa947648c91188ddc02e
andrew.farkas <aj.farkas@gsa.gov>
2025-08-01 12:53:37 -0400

    Portal #222 disable auto-deploy on prod
    * Portal #222 disable auto-deploy on prod
    
    See merge request https://gitlab.login.gov/lg/identity-dashboard/-/merge_requests/326

--- spec/policies/service_provider_updater_policy_spec.rb
+++ spec/policies/service_provider_updater_policy_spec.rb
@@ -4,8 +4,14 @@ describe ServiceProviderUpdaterPolicy do
   let(:user) { build(:user) }
 
   permissions :publish? do
-    it 'allows any user' do
+    it 'allows any user on sandbox envs' do
+      allow(IdentityConfig.store).to receive(:prod_like_env).and_return(false)
       expect(described_class).to permit(user)
+    end
+
+    it 'forbids all users on prod-like envs' do
+      allow(IdentityConfig.store).to receive(:prod_like_env).and_return(true)
+      expect(described_class).to_not permit(user)
     end
 
     it 'forbids without a user' do
