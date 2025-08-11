7156e433d52a248efb8028cfdba7582a32883bbf
AJ Farkas <aj.farkas@gsa.gov>
2025-08-11 11:11:17 -0400

    add link URLs and tests

---
+++ spec/features/footer_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+feature 'Footer' do
+  let(:user) { create(:user) }
+  let(:footer) { page.find('footer') }
+
+  before do
+    login_as(user)
+    visit root_path
+  end
+
+  context 'main section' do
+    let(:section) { footer.find('.footer-navigation') }
+
+    scenario 'should include the Login.gov logo' do
+      expect(section).to have_css('[alt="Login.gov logo"]')
+    end
+
+    scenario 'should include system status link' do
+      status_link = section.find('[href="https://status.login.gov"]')
+
+      expect(status_link.tag_name).to eq('a')
+      expect(status_link).to have_content('Login.gov system status')
+    end
+  end
+
+  context 'links section' do
+    let(:section) { footer.find('.usa-identifier') }
+
+    scenario 'should include GSA link' do
+      gsa_link = section.find('[href="https://www.gsa.gov"]')
+
+      expect(gsa_link.tag_name).to eq('a')
+      expect(gsa_link).to have_content('US General Services Administration')
+    end
+
+    scenario 'should include Dev Docs link' do
+      docs_link = section.find('[href="https://developers.login.gov/"]')
+
+      expect(docs_link.tag_name).to eq('a')
+      expect(docs_link).to have_content('Developer Guide')
+    end
+    scenario 'should include GSA link' do
+      gsa_link = section.find('[href="https://developers.login.gov/support/#contacting-partner-support"]')
+
+      expect(gsa_link.tag_name).to eq('a')
+      expect(gsa_link).to have_content('Contact')
+    end
+    scenario 'should include GSA link' do
+      gsa_link = section.find('[href="https://www.login.gov/policy/"]')
+
+      expect(gsa_link.tag_name).to eq('a')
+      expect(gsa_link).to have_content('Privacy & security')
+    end
+    scenario 'should include GSA link' do
+      gsa_link = section.find('[href="https://www.login.gov/accessibility/"]')
+
+      expect(gsa_link.tag_name).to eq('a')
+      expect(gsa_link).to have_content('Accessibility statement')
+    end
+  end
+end
