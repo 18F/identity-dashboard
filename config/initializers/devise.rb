3b641fb8d88912057839abb47763e5019dc5ef9b
AJ Farkas <aj.farkas@gsa.gov>
2025-08-28 16:39:27 -0400

    Portal #244 set Production timeout to 15min

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -158,7 +158,11 @@ Devise.setup do |config|
   # ==> Configuration for :timeoutable
   # The time you want to timeout the user session without activity. After this
   # time the user will be asked for credentials again. Default is 30 minutes.
-  config.timeout_in = 8.hours
+  if Rails.env == 'production' || IdentityConfig.store.prod_like_env
+    config.timeout_in = 15.minutes
+  else 
+    config.timeout_in = 8.hours
+  end
 
   # ==> Configuration for :lockable
   # Defines which strategy will be used to lock an account.
