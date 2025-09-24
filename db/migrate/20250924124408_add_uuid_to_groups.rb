a3c5dcaf5791ee3ce64dbd08f90bf66eff1d15d0
Jeremy Curcio <jeremy.curcio@gsa.gov>
2025-09-24 11:07:09 -0400

    Add index to make sure uuid is unique

--- db/migrate/20250924124408_add_uuid_to_groups.rb
+++ db/migrate/20250924124408_add_uuid_to_groups.rb
@@ -3,6 +3,9 @@ require 'securerandom'
 class AddUuidToGroups < ActiveRecord::Migration[7.2]
   def up
     add_column :groups, :uuid, :string
+    add_index :groups, :uuid, unique: true
+
+    change_column_default :groups, :uuid, from: nil, to: SecureRandom.uuid
 
     # Update existing records with random UUIDs
     Team.all.each do |team| 
