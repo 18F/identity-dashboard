e788407a9a49184fc9ffa16d4629f6624ebc00e5
AJ Farkas <aj.farkas@gsa.gov>
2025-09-24 10:51:56 -0400

    test extract model

--- app/models/extract.rb
+++ app/models/extract.rb
@@ -34,7 +34,7 @@ class Extract
   private
 
   def file_and_or_list
-    if @criteria_list.empty? && !@file_criteria
+    if list_criteria.empty? && file_criteria.empty?
       errors.add(:criteria_file, 'or Criteria List are required.')
       errors.add(:criteria_list, 'or Criteria File are required.')
     end
--- spec/models/extract_spec.rb
+++ spec/models/extract_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe Extract do
   let(:user) { create(:user, :logingov_admin) }
+  let(:issuer_file) { fixture_file_upload('issuers.txt', 'text/plain') }
 
   describe 'Validations' do
     it { should validate_presence_of(:ticket) }
@@ -9,19 +10,79 @@ describe Extract do
     it { should validate_inclusion_of(:search_by).in_array(%w[teams issuers]) }
 
     it 'should validate that file or list criteria are included' do
-      test_extract = build(:extract, {
+      with_list = build(:extract, {
+        ticket: '0',
+        search_by: 'teams',
+        criteria_list: '1 ,2',
+      } )
+      with_file = build(:extract, {
         ticket: '1',
+        search_by: 'issuers',
+        criteria_file: issuer_file,
+      } )
+
+      expect(with_list).to be_valid
+      expect(with_file).to be_valid
+      expect(with_list.errors).to be_empty
+      expect(with_file.errors).to be_empty
+    end
+
+    it 'should add errors when file and list criteria are excluded' do
+      test_extract = build(:extract, {
+        ticket: '0',
         search_by:'teams',
         criteria_list: '',
       } )
 
       expect(test_extract).to_not be_valid
+      expect(test_extract.errors).to include(:criteria_list, :criteria_file)
     end
   end
 
   describe '#file_criteria' do
+    it 'should return an empty array when there is no file uploaded' do
+      extract = build(:extract, {
+        ticket: '0',
+        search_by: 'teams',
+        criteria_list: '1',
+      } )
+
+      expect(extract.file_criteria).to eq([])
+    end
+
+    it 'should return an array of the issuer strings in the criteria_file' do
+      extract = build(:extract, { 
+        ticket: '0',
+        search_by: 'issuers',
+        criteria_file: issuer_file,
+      } )
+
+      expect(extract.file_criteria).to eq(
+        ['issuer:one', 'issuer:two', 'issuer:three'],
+      )
+    end
   end
 
   describe '#list_criteria' do
+    it 'should return an empty array when there is no file uploaded' do
+      extract = Extract.new(
+        ticket: '0',
+        search_by: 'issuers',
+        criteria_file: issuer_file,
+      )
+
+      expect(extract.list_criteria).to eq([])
+    end
+
+    it 'should return an array of the team ids in the criteria_list' do
+      extract = Extract.new(
+        ticket: '0',
+        search_by: 'teams',
+        criteria_list: '1,  2 3
+        4',
+      )
+
+      expect(extract.list_criteria).to eq(['1', '2', '3', '4'])
+    end
   end
 end
