d419ab08e98bcc0b08039e4b625a75e42f60c44c
Jeremy Curcio <jeremy.curcio@gsa.gov>
2025-09-26 14:44:12 -0400

    Add and use strong_migrations gem for generating UUIDs
    # Conflicts:
    #	Gemfile.lock

--- db/migrate/20250924124408_add_uuid_to_groups.rb
+++ db/migrate/20250924124408_add_uuid_to_groups.rb
@@ -1,13 +1,18 @@
 require 'securerandom'
 
 class AddUuidToGroups < ActiveRecord::Migration[7.2]
+  disable_ddl_transaction!
+
   def up
     add_column :groups, :uuid, :string
-    add_index :groups, :uuid, unique: true
+    add_index :groups, :uuid, unique: true, algorithm: :concurrently
 
     # Update existing records with random UUIDs
-    Team.all.each do |team| 
-      team.update(uuid: SecureRandom.uuid)
+    Team.unscoped.in_batches(of: 100) do |relation|
+      relation.where(uuid: nil).each do |team| 
+        team.update uuid: SecureRandom.uuid
+      end
+      sleep(0.01) # throttle
     end
   end
 
