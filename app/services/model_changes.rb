cf4aa41ae5f096eb58140fc54aabb0389a2b8ffd
Davida Marion <davida.marion@gsa.gov>
2025-10-07 12:56:46 -0400

    Add ModelChanges documentation

--- app/services/model_changes.rb
+++ app/services/model_changes.rb
@@ -1,3 +1,6 @@
+# A module to extract changes from ActiveRecord models for logging purposes.
+# This is used in controllers to log changes to models like ServiceProvider, Team, and TeamMembership.
+# It captures the previous changes made to a record and formats them into a hash suitable for logging.
 module ModelChanges
   def changes_to_log(record)
     changes = record.previous_changes.empty? ? record.as_json : {}
