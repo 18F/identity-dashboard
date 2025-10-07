06f8ac748cc59836659c9de8375f952a730e270a
Davida Marion <davida.marion@gsa.gov>
2025-10-07 13:56:00 -0400

    Fix lint

--- app/services/model_changes.rb
+++ app/services/model_changes.rb
@@ -1,5 +1,5 @@
 # A module to extract changes from ActiveRecord models for logging purposes.
-# This is used in controllers to log changes to models like ServiceProvider, Team, and 
+# This is used in controllers to log changes to models like ServiceProvider, Team, and
 # TeamMembership.
 # It captures the previous changes made to a record and formats them into a hash
 # suitable for logging.
