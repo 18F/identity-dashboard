58c534643dbd548a38af147fc5471d29817df822
AJ Farkas <aj.farkas@gsa.gov>
2025-10-10 15:40:30 -0400

    small fixes

--- app/policies/service_provider_updater_policy.rb
+++ app/policies/service_provider_updater_policy.rb
@@ -1,4 +1,4 @@
-# Permission policy for the ServiceProviderUpdater
+# Permission policy for writing ServiceProvider updates to staging IdP DB
 class ServiceProviderUpdaterPolicy < BasePolicy
   def publish?
     !!user && !IdentityConfig.store.prod_like_env
