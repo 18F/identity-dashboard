88a9b5e6bc04feee4f43557c750ee68a70376743
howard.miller <howard.miller@gsa.gov>
2025-10-14 16:18:11 -0400

    Add importing of extracted data
    * Add tests, including for a bug that showed up in today's demo
    
    * Invert default action for `extracts:import` rake task
    
    * Add dry run and other CLI interactions for extracts:import task
    
    * Remove logo storage key from extractor, update test fixture to match
    
    * Add some helpful output to extracts:import rake task
    
    * Require UUIDs for extract/import and incorporate code review feedback
    
    * Allow extracts controller to conditionally export UUIDs
    
    * Add `ExtractImporter` class and `extracts:import` rake task
    
    See merge request https://gitlab.login.gov/lg/identity-dashboard/-/merge_requests/380

--- spec/factories/team.rb
+++ spec/factories/team.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :team do
     sequence(:name) { |n| "department_name-#{n}" }
     sequence(:description) { |n| "description-#{n}" }
+    sequence(:uuid) { SecureRandom.uuid }
     agency
   end
 end
