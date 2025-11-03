23dc4e7d84139f2ee213e5f46d5d5d545367dffc
Jeremy Curcio <jeremy.curcio@gsa.gov>
2025-11-03 13:15:03 -0500

    Prefix the event

--- app/services/log_events.rb
+++ app/services/log_events.rb
@@ -110,7 +110,7 @@ module LogEvents
   # Log when a user clicks the back button
   # @params [String] Name of step that back button was pressed on
   def wizard_back_pressed(step_name:)
-    track_event('wizard_back_pressed', step_name:)
+    track_event('partner_portal_wizard_back_pressed', step_name:)
   end
 
   # Analytics log when a partner selects Next in the Guided Flow Wizard
