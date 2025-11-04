02568f997b9cbae3c64f9885b26eda69a16e130c
Howard M. Miller <howard.miller@gsa.gov>
2025-11-04 16:15:38 -0500

    Add intial RedirectController test for pair programming

---
+++ spec/controllers/redirect_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe RedirectController do
+  it 'can redirect a documentation link to the right URL' do
+    get :show, params: { destination: '/' }
+    expect(response).to have_http_status(:moved_permanently)
+    expect(response).to redirect_to('https://developers.login.gov/')
+  end
+end
