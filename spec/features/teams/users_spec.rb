f4499dd4f87ff625eb67b484d44e505265ff36a7
AJ Farkas <aj.farkas@gsa.gov>
2025-11-13 12:07:41 -0500

    env-specific roles tests

--- spec/features/teams/users_spec.rb
+++ spec/features/teams/users_spec.rb
@@ -140,6 +140,8 @@ describe 'users' do
         fill_in 'Email', with: random_email
         click_on 'Add'
         expect(page).to have_content(I18n.t('teams.users.create.success', email: random_email))
+        click_on 'Back'
+        expect(find('tr', :text => random_email)).to have_content(I18n.t('role_names.sandbox.partner_readonly'))
         new_membership = TeamMembership.find_by(team: team, user: User.find_by(email: random_email))
         expect(new_membership.role_name).to eq 'partner_readonly'
       end
@@ -151,6 +153,8 @@ describe 'users' do
         fill_in 'Email', with: random_email
         click_on 'Add'
         expect(page).to have_content(I18n.t('teams.users.create.success', email: random_email))
+        click_on 'Back'
+        expect(find('tr', :text => random_email)).to have_content(I18n.t('role_names.sandbox.partner_developer'))
         new_membership = TeamMembership.find_by(team: team, user: User.find_by(email: random_email))
         expect(new_membership.role_name).to eq 'partner_developer'
       end
@@ -172,6 +176,8 @@ describe 'users' do
       fill_in 'Email', with: random_email
       click_on 'Add'
       expect(page).to have_content(I18n.t('teams.users.create.success', email: random_email))
+      click_on 'Back'
+      expect(find('tr', :text => random_email)).to have_content(I18n.t('role_names.sandbox.partner_admin'))
       new_team_membership = TeamMembership.find_by(
         user: User.find_by(email: random_email),
         team: empty_team,
--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -10,4 +10,40 @@ RSpec.describe Role, type: :model do
     active_roles = Role::ROLES_NAMES.map { |name| Role.find_by(name:) }
     expect(active_roles.map(&:name)).to eq(Role::ROLES_NAMES)
   end
+
+  context 'on sandbox' do
+    before do
+      allow(IdentityConfig.store).to receive(:prod_like_env).and_return(false)
+    end
+
+    describe '#active_roles_names' do
+      it 'includes appropriate friendly names' do
+        roles_hash = {
+          'logingov_admin' => 'Login.gov Admin',
+          'partner_admin' => 'Sandbox Partner Admin',
+          'partner_developer' => 'Sandbox Team Dev',
+          'partner_readonly' => 'Team Readonly',
+        }
+        expect(Role.new.active_roles_names).to eq(roles_hash)
+      end
+    end
+  end
+
+  context 'on production' do
+    before do
+      allow(IdentityConfig.store).to receive(:prod_like_env).and_return(true)
+    end
+
+    describe '#active_roles_names' do
+      it 'includes appropriate friendly names' do
+        roles_hash = {
+          'logingov_admin' => 'Login.gov Admin',
+          'partner_admin' => 'Production Team Admin',
+          'partner_developer' => 'Production Team Dev',
+          'partner_readonly' => 'Team Readonly',
+        }
+        expect(Role.new.active_roles_names).to eq(roles_hash)
+      end
+    end
+  end
 end
