1b24050bcdace965df9507122d8be31bad61339f
Nadya Primak <nadya.primak@gsa.gov>
2025-11-17 17:08:13 -0500

    Revert "code review response"
    This reverts commit 8c7f6fe5a877575b1ed0823a12ad34106c34ad18.

--- app/controllers/docs_redirect_controller.rb
+++ app/controllers/docs_redirect_controller.rb
@@ -3,22 +3,21 @@ class DocsRedirectController < AuthenticatedController
   def show
     @destination = params[:destination] || ''
     unless @destination.blank?
-      # allow alphanumeric, forward slash, hash, underscore, and hyphen
-      @destination = @destination.tr('^A-Za-z0-9/#_-', '')
+      # remove leading slash + allow alphanumeric, forward slash, hash, underscore, and hyphen
+      @destination = @destination.sub(%r{^/}, '').tr('^A-Za-z0-9/#_-', '')
     end
-    destination_url = "https://developers.login.gov#{@destination}"
-    log_redirect(destination_url)
-    redirect_to destination_url,
+    log_redirect
+    redirect_to "https://developers.login.gov/#{@destination}",
                 status: :moved_permanently,
                 allow_other_host: true
   end
 
   private
 
-  def log_redirect(url)
+  def log_redirect
     log.redirect(
       origin_url: request.referer || '',
-      destination_url: url,
+      destination_url: "https://developers.login.gov/#{@destination}",
     )
   end
 end
