aed4bb50330e1f5f47972916a0edf2f435f31e2e
Jeremy Curcio <jeremy.curcio@gsa.gov>
2025-12-05 14:28:18 -0500

    Move the id or uuid lookup to application_record so as to utilize DRY

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,13 @@
 class ApplicationRecord < ActiveRecord::Base # :nodoc:
   self.abstract_class = true
+
+  UUID_REGEX = /\A[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\z/i
+
+  def self.find_by_id_or_uuid(identifier)
+    if UUID_REGEX.match?(identifier.to_s)
+      find_by(uuid: identifier)
+    else
+      find_by(id: identifier)
+    end
+  end
 end
