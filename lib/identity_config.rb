fd204dc5880e03356f410358ae30bcbf344bfb55
andrew.farkas <aj.farkas@gsa.gov>
2026-01-12 16:01:54 -0500

    Portal #315 remove legacy help_text_options flag
    * Portal #315 remove help_text flag
    
    See merge request https://gitlab.login.gov/lg/identity-dashboard/-/merge_requests/489

--- lib/identity_config.rb
+++ lib/identity_config.rb
@@ -42,7 +42,6 @@ class IdentityConfig
       # Feature Flags, options expected to be higher churn than the above settings
       config.add(:api_token_required_enabled, type: :boolean)
       config.add(:edit_button_uses_service_config_wizard, type: :boolean, allow_nil: true)
-      config.add(:help_text_options_feature_enabled, type: :boolean)
     end
     @store = Identity::Hostdata.config
     # rubocop:enable Metrics/BlockLength
