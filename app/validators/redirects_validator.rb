8e19b40c883f001b3cb3e410caf898ea7b072033
Jeremy Curcio <jeremy.curcio@gsa.gov>
2026-01-28 13:10:12 -0500

    Better URI validations

--- app/validators/redirects_validator.rb
+++ app/validators/redirects_validator.rb
@@ -4,12 +4,18 @@
 # unless that value is unchanged. The latter rule allows partners to add
 # URIs while keeping an existing `localhost` that was applied by an admin.
 class RedirectsValidator < IdentityValidations::AllowedRedirectsValidator
+  VALID_HOST_PATTERN = /\A[a-z0-9\-_.]+\z/i
+
   def validate(record)
     super
     self.attribute ||= :redirect_uris
     uris = get_attribute(record)
 
     return if uris.blank?
+
+    Array(uris).each do |uri_string|
+      check_valid_host(record, uri_string)
+    end
 
     # Only validate if the attribute is changing
     return if attribute_unchanged(record, attribute)
@@ -23,13 +29,28 @@ class RedirectsValidator < IdentityValidations::AllowedRedirectsValidator
 
   def attribute_unchanged(record, attribute)
     changed_form_data = record.changes['wizard_form_data']
+    attr_key = attribute.to_s
     !changed_form_data || (
-      changed_form_data[0] && changed_form_data[0][attribute] == changed_form_data[1][attribute]
+      changed_form_data[0] && changed_form_data[0][attr_key] == changed_form_data[1][attr_key]
     )
+  end
+
+  def check_valid_host(record, uri_string)
+    validating_uri = IdentityValidations::ValidatingURI.new(uri_string)
+    return unless validating_uri.parseable?
+
+    host = validating_uri.uri.host
+    return if host.blank?
+
+    return if host.match?(VALID_HOST_PATTERN)
+
+    record.errors.add(attribute, "#{uri_string} has an invalid host")
   end
 
   def check_nonadmin_localhost_redirect(record, uri_string)
     validating_uri = IdentityValidations::ValidatingURI.new(uri_string)
+    return unless validating_uri.parseable?
+
     user = User.find record.user_id
 
     # check if a nonadmin is using localhost on a prod_ready config
