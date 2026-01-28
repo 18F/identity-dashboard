e0db97073bdd805a7e4f44aca4a5afac95589343
AJ Farkas <aj.farkas@gsa.gov>
2026-01-28 16:27:36 -0500

    Portal #370 only allow banners to have .gov and internal links

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -5,6 +5,7 @@ class Banner < ApplicationRecord
             comparison: { greater_than: :start_date, message: 'must be after start date' },
             if: :start_date?,
             allow_blank: true
+  validate :link_allowed
 
   def started?
     start_date ? start_time < Time.zone.now : true
@@ -24,5 +25,18 @@ class Banner < ApplicationRecord
 
   def end_time
     end_date.end_of_day
+  end
+
+  private
+
+  def link_allowed
+    href = message.match(/href=['|"]([a-zA-Z0-9\/:\-_.?#&%]+)['|"]/)
+    return unless href
+
+    host = URI(href[1]).host
+    return true if !host || host.end_with?('.gov')
+
+    errors.add(:message, "link has disallowed host: #{host}")
+    false
   end
 end
