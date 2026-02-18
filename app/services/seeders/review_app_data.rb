dabaa2b33f5ec826aaefbf7551e1c2c2bdb3c360
Jeremy Curcio <jeremy.curcio@gsa.gov>
2026-02-18 15:54:02 -0500

    Reduce complexity a bit, make rubocop less warny

--- app/services/seeders/review_app_data.rb
+++ app/services/seeders/review_app_data.rb
@@ -47,28 +47,53 @@ class Seeders::ReviewAppData < Seeders::BaseSeeder
   def assign_memberships
     Role::ROLES_NAMES.each do |role_name|
       user = User.find_by(email: "#{role_name.tr('_', '-')}@gsa.gov")
+      assign_to_internal_team(user, role_name) if role_name.start_with?('logingov_')
+      assign_to_review_teams(user, role_name)
+    end
+  end
 
-      # Login.gov staff get added to internal team with their actual role
-      if role_name.start_with?('logingov_') && Team.internal_team && !TeamMembership.exists?(
-        user: user, team: Team.internal_team,
-      )
-        TeamMembership.create!(user: user, team: Team.internal_team, role_name: role_name)
-        logger.info "Assigned #{user.email} to #{Team.internal_team.name} as #{role_name}"
-      end
+  def assign_to_internal_team(user, role_name)
+    return unless Team.internal_team
+    return if TeamMembership.exists?(user: user, team: Team.internal_team)
 
-      # Everyone gets partner roles on the review teams
-      partner_role = role_name.sub(/^logingov_/, 'partner_')
-      Team.where(name: ['Production Team', 'Sandbox Team']).find_each do |team|
-        next if TeamMembership.exists?(user: user, team: team)
+    TeamMembership.create!(user: user, team: Team.internal_team, role_name: role_name)
+    logger.info "Assigned #{user.email} to #{Team.internal_team.name} as #{role_name}"
+  end
 
-        TeamMembership.create!(user: user, team: team, role_name: partner_role)
-        logger.info "Assigned #{user.email} to #{team.name} as #{partner_role}"
-      end
+  def assign_to_review_teams(user, role_name)
+    partner_role = role_name.sub(/^logingov_/, 'partner_')
+    Team.where(name: ['Production Team', 'Sandbox Team']).find_each do |team|
+      next if TeamMembership.exists?(user: user, team: team)
+
+      TeamMembership.create!(user: user, team: team, role_name: partner_role)
+      logger.info "Assigned #{user.email} to #{team.name} as #{partner_role}"
     end
   end
 
   def create_service_providers
-    base_attrs = {
+    [
+      { name: 'Prod OIDC', prod: true, protocol: :openid_connect_pkce },
+      { name: 'Prod SAML', prod: true, protocol: :saml },
+      { name: 'Sandbox OIDC', prod: false, protocol: :openid_connect_pkce },
+      { name: 'Sandbox SAML', prod: false, protocol: :saml },
+    ].each { |config| create_service_provider(config) }
+  end
+
+  def create_service_provider(config)
+    issuer = "urn:gov:gsa:reviewapp:#{config[:name].downcase.tr(' ', ':')}"
+    return if ServiceProvider.exists?(issuer: issuer)
+
+    ServiceProvider.create!(service_provider_attrs(config, issuer))
+    logger.info "Created service provider: #{config[:name]}"
+  end
+
+  def service_provider_attrs(config, issuer)
+    attrs = {
+      issuer: issuer,
+      friendly_name: config[:name],
+      identity_protocol: config[:protocol],
+      prod_config: config[:prod],
+      team: Team.find_by(name: config[:prod] ? 'Production Team' : 'Sandbox Team'),
       user: User.find_by(email: 'logingov-admin@gsa.gov'),
       agency_id: Seeders::AgencySeeder.internal_agency_data[:id],
       attribute_bundle: %w[email],
@@ -76,33 +101,12 @@ class Seeders::ReviewAppData < Seeders::BaseSeeder
       ial: 1,
       default_aal: 1,
     }
-
-    configs = [
-      { name: 'Prod OIDC', prod: true, protocol: :openid_connect_pkce },
-      { name: 'Prod SAML', prod: true, protocol: :saml },
-      { name: 'Sandbox OIDC', prod: false, protocol: :openid_connect_pkce },
-      { name: 'Sandbox SAML', prod: false, protocol: :saml },
-    ]
-
-    configs.each do |c|
-      issuer = "urn:gov:gsa:reviewapp:#{c[:name].downcase.tr(' ', ':')}"
-      next if ServiceProvider.exists?(issuer: issuer)
-
-      sp = ServiceProvider.new(base_attrs.merge(
-        issuer: issuer,
-        friendly_name: c[:name],
-        identity_protocol: c[:protocol],
-        prod_config: c[:prod],
-        team: Team.find_by(name: c[:prod] ? 'Production Team' : 'Sandbox Team'),
-      ))
-      if c[:protocol] == :saml
-        sp.acs_url = 'https://example.gov/saml/acs'
-        sp.return_to_sp_url = 'https://example.gov/return'
-      else
-        sp.redirect_uris = ['https://example.gov/callback']
-      end
-      sp.save!
-      logger.info "Created service provider: #{c[:name]}"
+    if config[:protocol] == :saml
+      attrs[:acs_url] = 'https://example.gov/saml/acs'
+      attrs[:return_to_sp_url] = 'https://example.gov/return'
+    else
+      attrs[:redirect_uris] = ['https://example.gov/callback']
     end
+    attrs
   end
 end
