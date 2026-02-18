aff615d36ebab00b81b95549af0a9d9f97d74e4a
Jeremy Curcio <jeremy.curcio@gsa.gov>
2026-02-18 14:54:56 -0500

    Seed all the things for review apps
    Users, team, team memberships, service providers, oh my!

---
+++ app/services/seeders/review_app_data.rb
@@ -0,0 +1,100 @@
+# Seeds test users, teams, and service providers for review app environments.
+class Seeders::ReviewAppData < Seeders::BaseSeeder
+  def seed
+    return unless ENV['KUBERNETES_REVIEW_APP']
+
+    create_users
+    create_teams
+    assign_memberships
+    create_service_providers
+    logger.info 'Seeded review app data'
+  end
+
+  private
+
+  def create_users
+    Role::ROLES_NAMES.each do |role_name|
+      email = "#{role_name.tr('_', '-')}@gsa.gov"
+      next if User.exists?(email: email)
+
+      User.create!(email: email)
+      logger.info "Created user: #{email}"
+    end
+  end
+
+  def create_teams
+    %w[Production Sandbox].each do |env|
+      name = "#{env} Team"
+      next if Team.exists?(name: name)
+
+      Team.create!(
+        name: name,
+        agency_id: Seeders::AgencySeeder.internal_agency_data[:id],
+        description: "Review app #{env.downcase} team",
+      )
+      logger.info "Created team: #{name}"
+    end
+  end
+
+  def assign_memberships
+    Role::ROLES_NAMES.each do |role_name|
+      user = User.find_by(email: "#{role_name.tr('_', '-')}@gsa.gov")
+
+      # Login.gov staff get added to internal team with their actual role
+      if role_name.start_with?('logingov_') && Team.internal_team
+        unless TeamMembership.exists?(user: user, team: Team.internal_team)
+          TeamMembership.create!(user: user, team: Team.internal_team, role_name: role_name)
+          logger.info "Assigned #{user.email} to #{Team.internal_team.name} as #{role_name}"
+        end
+      end
+
+      # Everyone gets partner roles on the review teams
+      partner_role = role_name.sub(/^logingov_/, 'partner_')
+      Team.where(name: ['Production Team', 'Sandbox Team']).find_each do |team|
+        next if TeamMembership.exists?(user: user, team: team)
+
+        TeamMembership.create!(user: user, team: team, role_name: partner_role)
+        logger.info "Assigned #{user.email} to #{team.name} as #{partner_role}"
+      end
+    end
+  end
+
+  def create_service_providers
+    base_attrs = {
+      user: User.find_by(email: 'logingov-admin@gsa.gov'),
+      agency_id: Seeders::AgencySeeder.internal_agency_data[:id],
+      attribute_bundle: %w[email],
+      help_text: { 'sign_in' => {}, 'sign_up' => {}, 'forgot_password' => {} },
+      ial: 1,
+      default_aal: 1,
+    }
+
+    configs = [
+      { name: 'Prod OIDC', prod: true, protocol: :openid_connect_pkce },
+      { name: 'Prod SAML', prod: true, protocol: :saml },
+      { name: 'Sandbox OIDC', prod: false, protocol: :openid_connect_pkce },
+      { name: 'Sandbox SAML', prod: false, protocol: :saml },
+    ]
+
+    configs.each do |c|
+      issuer = "urn:gov:gsa:reviewapp:#{c[:name].downcase.tr(' ', ':')}"
+      next if ServiceProvider.exists?(issuer: issuer)
+
+      sp = ServiceProvider.new(base_attrs.merge(
+        issuer: issuer,
+        friendly_name: c[:name],
+        identity_protocol: c[:protocol],
+        prod_config: c[:prod],
+        team: Team.find_by(name: c[:prod] ? 'Production Team' : 'Sandbox Team'),
+      ))
+      if c[:protocol] == :saml
+        sp.acs_url = 'https://example.gov/saml/acs'
+        sp.return_to_sp_url = 'https://example.gov/return'
+      else
+        sp.redirect_uris = ['https://example.gov/callback']
+      end
+      sp.save!
+      logger.info "Created service provider: #{c[:name]}"
+    end
+  end
+end
---
+++ spec/services/seeders/review_app_data_spec.rb
@@ -0,0 +1,91 @@
+require 'rails_helper'
+
+RSpec.describe Seeders::ReviewAppData do
+  let(:logger) { Rails.logger }
+
+  before do
+    allow(logger).to receive(:info).with(any_args)
+  end
+  
+  context 'without KUBERNETES_REVIEW_APP set' do
+    it 'does nothing' do
+      expect { described_class.new.seed }.to_not change { User.count }
+    end
+  end
+
+  context 'with KUBERNETES_REVIEW_APP set' do
+    before do
+      allow(ENV).to receive(:[]).and_call_original
+      allow(ENV).to receive(:[]).with('KUBERNETES_REVIEW_APP').and_return('true')
+    end
+
+    it 'seeds users' do
+      expect { described_class.new.seed }.to change { User.count }.by 5
+
+      expect(User.find_by(email: 'logingov-admin@gsa.gov')).to be_present
+      expect(User.find_by(email: 'logingov-readonly@gsa.gov')).to be_present
+      expect(User.find_by(email: 'partner-admin@gsa.gov')).to be_present
+      expect(User.find_by(email: 'partner-developer@gsa.gov')).to be_present
+      expect(User.find_by(email: 'partner-readonly@gsa.gov')).to be_present
+
+      expect(logger).to have_received(:info).with('Created user: logingov-admin@gsa.gov')
+      expect(logger).to have_received(:info).with('Created user: logingov-readonly@gsa.gov')
+      expect(logger).to have_received(:info).with('Created user: partner-admin@gsa.gov')
+      expect(logger).to have_received(:info).with('Created user: partner-developer@gsa.gov')
+      expect(logger).to have_received(:info).with('Created user: partner-readonly@gsa.gov')
+    end
+
+    it 'seeds teams' do
+      expect { described_class.new.seed }.to change { Team.count }.by 2
+
+      expect(Team.find_by(name: 'Production Team')).to be_present
+      expect(Team.find_by(name: 'Sandbox Team')).to be_present
+
+      expect(logger).to have_received(:info).with('Created team: Production Team')
+      expect(logger).to have_received(:info).with('Created team: Sandbox Team')
+    end
+
+    it 'seeds team_memberships' do
+      described_class.new.seed
+      
+      expect(logger).to have_received(:info)
+        .with('Assigned logingov-admin@gsa.gov to Login.gov Internal Team as logingov_admin')
+      expect(logger).to have_received(:info)
+        .with('Assigned logingov-admin@gsa.gov to Production Team as partner_admin')
+      expect(logger).to have_received(:info)
+        .with('Assigned logingov-admin@gsa.gov to Sandbox Team as partner_admin')
+      expect(logger).to have_received(:info)
+        .with('Assigned logingov-readonly@gsa.gov to Login.gov Internal Team as logingov_readonly')
+      expect(logger).to have_received(:info)
+        .with('Assigned logingov-readonly@gsa.gov to Production Team as partner_readonly')
+      expect(logger).to have_received(:info)
+        .with('Assigned logingov-readonly@gsa.gov to Sandbox Team as partner_readonly')
+      expect(logger).to have_received(:info)
+        .with('Assigned partner-admin@gsa.gov to Production Team as partner_admin')
+      expect(logger).to have_received(:info)
+        .with('Assigned partner-admin@gsa.gov to Sandbox Team as partner_admin')
+      expect(logger).to have_received(:info)
+        .with('Assigned partner-developer@gsa.gov to Production Team as partner_developer')
+      expect(logger).to have_received(:info)
+        .with('Assigned partner-developer@gsa.gov to Sandbox Team as partner_developer')
+      expect(logger).to have_received(:info)
+        .with('Assigned partner-readonly@gsa.gov to Production Team as partner_readonly')
+      expect(logger).to have_received(:info)
+        .with('Assigned partner-readonly@gsa.gov to Sandbox Team as partner_readonly')
+
+    end
+
+    it 'seeds configurations' do
+      expect { described_class.new.seed }.to change { ServiceProvider.count }.by 4
+        expect(logger).to have_received(:info).with('Created service provider: Prod OIDC')
+        expect(logger).to have_received(:info).with('Created service provider: Prod SAML')
+        expect(logger).to have_received(:info).with('Created service provider: Sandbox OIDC')
+        expect(logger).to have_received(:info).with('Created service provider: Sandbox SAML')
+    end
+
+    it 'is idempotent' do
+      2.times { described_class.new.seed }
+      expect(User.where(email: 'partner-admin@gsa.gov').count).to eq(1)
+    end
+  end
+end
