583c40b17e3d2ed1e74c1b4bd5a61686cf9d4c5c
Jeremy Curcio <jeremy.curcio@gsa.gov>
2026-02-26 09:27:50 -0500

    Rubocop

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper # :nodoc:
-  include Chartkick::Helper 
-  
+  include Chartkick::Helper
+
   def navigation_link_to(text, route)
     link_to text, route, class: [current_page?(route) && 'usa-current', 'usa-nav__link']
   end
