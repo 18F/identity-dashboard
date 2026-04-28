0935e4acdcf1353b6b2b6be2661281082fb7258f
AJ Farkas <aj.farkas@gsa.gov>
2026-04-28 12:14:44 -0400

    test download button exists

--- spec/features/analytics_report_spec.rb
+++ spec/features/analytics_report_spec.rb
@@ -14,12 +14,23 @@ describe 'reporting feature basics' do
     )
   end
 
-  it 'logingov admin can get the page', :js do
-    expect(test_sp).to be_valid
-    login_as logingov_admin
-    visit analytics_path
-    selection_texts = find_all('select').map(&:text)
-    expect(selection_texts).to include(logingov_admin.teams.first.name)
-    expect(selection_texts).to include(test_sp.friendly_name)
+  context 'as logingov_admin' do
+    before do
+      login_as logingov_admin
+    end
+
+    it 'can view appropriate team and issuer options' do
+      expect(test_sp).to be_valid
+      visit analytics_path
+      selection_texts = find_all('select').map(&:text)
+      expect(selection_texts).to include(logingov_admin.teams.first.name)
+      expect(selection_texts).to include(test_sp.friendly_name)
+    end
+
+    it 'contains a link to download a CSV' do
+      expect(test_sp).to be_valid
+      visit analytics_path
+      expect(page).to have_link('Download CSV', href: analytics_download_path)
+    end
   end
 end
