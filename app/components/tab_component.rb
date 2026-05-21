e88df245884dce5eb84245acd86d98e9665b4dbd
AJ Farkas <aj.farkas@gsa.gov>
2026-05-21 11:30:53 -0400

    render some panels

--- app/components/tab_component.rb
+++ app/components/tab_component.rb
@@ -1,8 +1,16 @@
 # A tab component to allow view switching
 class TabComponent < ViewComponent::Base
-  attr_reader :tabs
+  attr_reader :tab_data, :opts
 
-  def initialize(tabs:)
-    @tabs = tabs
+  # @param [Array] tab_data objects containing tab and panel info
+  #  [{
+  #    title: 'String tab title',
+  #    id: 'String panel element ID',
+  #    content: 'HTML/Partial',
+  #  }],
+  # @param [Object] opts options to be passed into HTML
+  #  { class_name: 'String' }
+  def initialize(tab_data:, opts: {})
+    @tab_data, @opts = tab_data, opts
   end
 end
