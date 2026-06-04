60a46227bb2ebf2a57832f5b6310a920c1e5a91a
AJ Farkas <aj.farkas@gsa.gov>
2026-06-04 15:23:14 -0400

    create `#add_tab` method on `TabComponent` rb

--- app/components/tab_component.rb
+++ app/components/tab_component.rb
@@ -1,32 +1,27 @@
 # A tab component to allow view switching
 #
-# @example render a basic 3-tab component
-# <%= render(TabComponent.new(tab_data: [
-#     { title: 'Tab One',
-#       id: 'tab1',
-#       content: render('components/feature_card', {
-#         icon: 'accessible_forward',
-#         title: 'footer.performance',
-#         body: 'forms.confirm_service_provider'}),
-#       focusable: false,
-#     },
-#     { title: 'Tab Two',
-#       id: 'tab2',
-#       content: render('components/feature_card', {
-#         icon: 'assessment',
-#         title: 'footer.no_fear_act',
-#         body: 'team_memberships.rbac_description'}),
-#       focusable: false,
-#     },
-#     { title: 'Tab Three',
-#       id: 'tab3',
-#       content: render('components/feature_card', {
-#         icon: 'electrical_services',
-#         title: 'footer.status',
-#         body: 'footer.disclaimer_html'}),
-#       focusable: true
-#     },
-#   ], opts: { class_name: 'test-me' })) %>
+# @example render a basic 3-tab component. Only final render should use <%=.
+# <% tab_component = TabComponent.new(
+#              opts: { class_name: 'test-me' }
+#            ).add_tab(title: 'Tab One', id: 'tab1') do %>
+#      <% render('components/feature_card', {
+#           icon: 'accessible_forward',
+#           title: 'footer.performance',
+#           body: 'forms.confirm_service_provider'}) %>
+# <% end %>
+# <% tab_component.add_tab(title: 'Tab Two', id: 'tab2') do %>
+#     <% render('components/feature_card', {
+#           icon: 'assessment',
+#           title: 'footer.no_fear_act',
+#           body: 'team_memberships.rbac_description'}) %>
+# <% end %>
+# <% tab_component.add_tab(title: 'Tab Three', id: 'tab', focusable: true) do %>
+#     <% render('components/feature_card', {
+#           icon: 'electrical_services',
+#           title: 'footer.status',
+#           body: 'footer.disclaimer_html'}) %>
+# <% end %>
+# <%= render tab_component %>
 class TabComponent < ViewComponent::Base
   attr_reader :tab_data, :opts
 
@@ -39,7 +34,16 @@ class TabComponent < ViewComponent::Base
   #  }],
   # @param [Object] opts options to be passed into HTML
   #  { class_name: String class name }
-  def initialize(tab_data:, opts: {})
+  def initialize(tab_data: [], opts: {})
     @tab_data, @opts = tab_data, opts
+  end
+
+  # @param [Object] tab_options any of tab_data options (see above)
+  # @yield [block] Tab panel contents (optional)
+  def add_tab(**tab_options)
+    tab_options[:content] = yield if block_given?
+    tab_data.push(tab_options)
+
+    self
   end
 end
