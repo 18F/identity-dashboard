745d77e5481978dde11b27bc68ed4bfe622b0ced
AJ Farkas <aj.farkas@gsa.gov>
2026-06-17 15:17:14 -0400

    fix a11y spec with new example tabs

--- app/components/tab_component.rb
+++ app/components/tab_component.rb
@@ -2,27 +2,24 @@
 #
 # @example render a basic 3-tab component. Only final render should use <%=.
 # <% tab_component = TabComponent.new(
-#      opts: { class_name: 'test-me' }
-#    ).add_tab(title: 'Tab One', id: 'tab1') do %>
-#      <% render('components/feature_card', {
-#        icon: 'accessible_forward',
-#        title: 'footer.performance',
-#        body: 'forms.confirm_service_provider'}) %>
-# <% end %>
-# <% tab_component.add_tab(title: 'Tab Two', id: 'tab2', focusable: true) do %>
-#   <% capture do %>
-#     <p class="usa-prose">This is the…</p>
-#     <p class="usa-prose">second tab panel!</p>
-#     <a class="usa-link" href="#tab3">Go to tab 3</a>
+#        opts: { class_name: 'test-me' }
+#      ).add_tab(title: 'Tab One', id: 'tab1') do %>
+#        <% render('components/logo_banner') %>
 #   <% end %>
-# <% end %>
-# <% tab_component.add_tab(title: 'Tab Three', id: 'tab3', focusable: true) do %>
-#   <% render('components/feature_card', {
-#     icon: 'electrical_services',
-#     title: 'footer.status',
-#     body: 'footer.disclaimer_html'}) %>
-# <% end %>
-# <%= render tab_component %>
+#   <% tab_component.add_tab(title: 'Tab Two', id: 'tab2', focusable: true) do %>
+#     <% capture do %>
+#       <p class="usa-prose">This is the…</p>
+#       <p class="usa-prose">second tab panel!</p>
+#       <a class="usa-link" href="#tab3">Go to tab 3</a>
+#     <% end %>
+#   <% end %>
+#   <% tab_component.add_tab(title: 'Tab Three', id: 'tab3', focusable: true) do %>
+#     <% render('components/step_progress', {
+#       steps: ['en', 'es', 'fr', 'zh'],
+#       current_step_index: 0,
+#       localization_base: 'locale_map'}) %>
+#   <% end %>
+#   <%= render tab_component %>
 class TabComponent < ViewComponent::Base
   attr_reader :tab_data, :opts
 
