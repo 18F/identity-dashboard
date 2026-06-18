304fcb7f5cd18578bb1f5f06f7c363a6888d880b
AJ Farkas <aj.farkas@gsa.gov>
2026-06-18 10:31:10 -0400

    analytic model spec

--- spec/models/analytic_spec.rb
+++ spec/models/analytic_spec.rb
@@ -41,5 +41,39 @@ RSpec.describe Analytic, type: :model do
 
       expect(analytic).to have_received(:is_valid?).once
     end
+
+    it 'returns true when all checks pass' do
+      expect(analytic.is_valid?).to be_truthy
+    end
+
+    it 'makes minimal checks when the config is invalid' do
+      allow(analytic).to receive(:config_valid?).and_return(false)
+
+      expect(analytic.is_valid?).to be_falsey
+
+      expect(analytic).to have_received(:config_valid?).once
+      expect(analytic).to_not have_received(:valid_date?)
+      expect(analytic).to_not have_received(:data_valid?)
+    end
+
+    it 'makes minimal checks when the date is invalid' do
+      allow(analytic).to receive(:valid_date?).and_return(false)
+
+      expect(analytic.is_valid?).to be_falsey
+
+      expect(analytic).to have_received(:config_valid?).once
+      expect(analytic).to have_received(:valid_date?).once
+      expect(analytic).to_not have_received(:data_valid?)
+    end
+
+    it 'makes correct checks when the data is invalid' do
+      allow(analytic).to receive(:data_valid?).and_return(false)
+
+      expect(analytic.is_valid?).to be_falsey
+
+      expect(analytic).to have_received(:config_valid?).once
+      expect(analytic).to have_received(:valid_date?).once
+      expect(analytic).to have_received(:data_valid?).once
+    end
   end
 end
