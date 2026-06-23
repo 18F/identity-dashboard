4cdaecc7c530fee6b693fce51e01e4cb6ce58061
andrew.farkas <aj.farkas@gsa.gov>
2026-06-23 13:20:32 -0400

    Portal #471 show mobile tab expand icon
    * Portal #471 show mobile tab expand icon
    
    See merge request https://gitlab.login.gov/lg/identity-dashboard/-/merge_requests/671

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -26,9 +26,11 @@ SecureHeaders::Configuration.default do |config|
   img_src = ["'self'", 'data:', "https://s3.#{IdentityConfig.store.aws_region}.amazonaws.com"]
 
   if Rails.env.development?
-    script_src.push('*.ssa.gov', 'ajax.googleapis.com')
-    style_src.push("'unsafe-inline'", '*.ssa.gov')
-    img_src.push('*.ssa.gov')
+    script_src.push('ajax.googleapis.com')
+    # uncomment for ANDI manual A11y testing
+    # script_src.push('*.ssa.gov')
+    # style_src.push("'unsafe-inline'", '*.ssa.gov')
+    # img_src.push('*.ssa.gov')
   end
 
   config.csp = {
