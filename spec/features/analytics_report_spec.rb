72181e76b07a68f9b20ed8277004229351a7f3fe
Jeremy Curcio <jeremy.curcio@gsa.gov>
2026-08-21 13:48:12 -0400

    Use around instead of before, so the specs don’t stay stuck in the time freeze

--- spec/features/analytics_report_spec.rb
+++ spec/features/analytics_report_spec.rb
@@ -123,9 +123,7 @@ describe 'reporting feature basics' do
       end
 
       context 'with dates computed from created_at' do
-        before { travel_to(Date.new(2025, 12, 15)) }
-
-        around(&:run)
+        around { |example| travel_to(Date.new(2025, 12, 15)) { example.run } }
 
         let(:test_sp) do
           create(
