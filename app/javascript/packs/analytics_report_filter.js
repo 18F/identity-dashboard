76e424d7f841884647f8605729685bb6d5710427
andrew.farkas <aj.farkas@gsa.gov>
2026-09-04 12:12:52 -0400

    Portal #597 safari report dates bug
    * remove unused code path and add comments to conditions
    
    * use JS to remove/add appropriate dropdown options
    
    * update dropdowns based on selected team/app
    
    * update tests
    
    * Portal #597 safari report dates bug
    
    changelog: Bug Fixes, Reports, Webkit browsers do not allow `display: none` on <options>
    
    See merge request https://gitlab.login.gov/lg/identity-dashboard/-/merge_requests/767

--- app/javascript/packs/analytics_report_filter.js
+++ app/javascript/packs/analytics_report_filter.js
@@ -5,6 +5,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const allSelects = document.getElementById('reports_form').querySelectorAll('select');
   const teamSelect = document.getElementById('analytic_team');
   const appSelect = document.getElementById('analytic_uuid');
+  const dateSelect = document.getElementById('analytic_date');
+  const allAppOptions = new Array(...appSelect.getElementsByTagName('option'));
+  const allDateOptions = new Array(...dateSelect.getElementsByTagName('option'));
 
   const onSelectChange = (ev) => {
     // Parent select element and children
@@ -13,30 +16,39 @@ document.addEventListener('DOMContentLoaded', () => {
     const optIds = opt.dataset.controls.split(',');
     // Child select element and children
     const nextSelect = allSelects[[...allSelects].indexOf(select) + 1];
-    const nextOptions = nextSelect.querySelectorAll('option');
-    const nextValueIsSet = opt.dataset.controls.includes(nextSelect.value);
+    const nextOptions = nextSelect === appSelect ? allAppOptions : allDateOptions;
+    const nextValue = nextSelect.value;
+    const nextValueIsSet = !!opt.dataset.controls.includes(nextValue);
     // Unless a specific option is selected, don't select a new option on child
     let optNeedsSetting = !!select.value.length;
     // Filter the child options
+    nextSelect.innerHTML = '';
     nextOptions.forEach((option) => {
-      if (optIds.indexOf(option.value) < 0) {
-        option.classList.add('display-none');
-      } else {
-        option.classList.remove('display-none');
-        // Set the child select value, and cascade if App
+      if (optIds.indexOf(option.value) >= 0) {
+        nextSelect.appendChild(option);
+        // Set the child select value, and cascade if App select
         if (optNeedsSetting || !nextValueIsSet) {
+          // condition: no default
           if (optNeedsSetting && !nextValueIsSet) {
             nextSelect.value = option.value;
           }
-          if (nextSelect === appSelect) {
+          // if the next Select element is Application, also update Dates
+          if (!nextValueIsSet && nextSelect === appSelect) {
             nextSelect.dispatchEvent(new Event('change'));
           }
+          // We updated selected option, so it doesn't need to be set again.
           optNeedsSetting = false;
+        }
+        // If the next value is preselected (ie by URL), set it and base the next
+        // set of options on this value.
+        if (nextValue === option.value && nextValueIsSet) {
+          nextSelect.value = option.value;
+          nextSelect.dispatchEvent(new Event('change'));
         }
       }
     });
   };
-
+  // Listen for changes to Team and Application Selects
   teamSelect.addEventListener('change', onSelectChange);
   appSelect.addEventListener('change', onSelectChange);
   // Filter URL-defined report on page load
