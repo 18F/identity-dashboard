086d27dcaa0a39591745d117f4e4c15e304955b7
Nadya Primak <nadya.primak@gsa.gov>
2026-09-09 13:29:27 -0400

    update test

--- spec/models/report/base_spec.rb
+++ spec/models/report/base_spec.rb
@@ -24,8 +24,12 @@ describe Report::Base do
         ['Existing Accounts', 23], ['Identity Verified Users', 103], ['Newly Proofed Users', 83],
         ['Preverified Users', 20], ['Authentications', 1501], ['Account Creation', 1],
         ['Authentic Driver\'s License', 1], ['Facial Matching Check', 2],
+<<<<<<< HEAD
         ['Valid Drivers License / Passport / etc.', 1],
         ['Identity Not Found (SSN / DOB / Deceased)', 3],
+=======
+        ['Valid Drivers License / Passport / etc.', 1], ['Identity Not Found (SSN / DOB / Deceased)', 3],
+>>>>>>> e167d36d (update test)
         ['Under Active Fraud Alert', 1], ['Suspicious Phone', 1], ['Phone Account Ownership', 0],
         ['Wrong Phone Type', 0], ['Rejected at Post Office', 1], ['Pending Fraud Review', 0],
         ['Adjudicated as Legitimate', 0], ['Preverified', 2], ['Remote Unattended', 61],
