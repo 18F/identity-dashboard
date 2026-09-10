a658390b58c88505e2b618df7befd876ebe9fe50
AJ Farkas <aj.farkas@gsa.gov>
2026-09-10 17:11:11 -0400

    Portal #603 Configure RackAttack throttling via IdentityConfig store
    changelog: Internal, Security, Configure RackAttack throttle via store

--- lib/identity_config.rb
+++ lib/identity_config.rb
@@ -39,6 +39,8 @@ class IdentityConfig
       config.add(:rack_timeout_service_timeout_seconds, type: :integer)
       config.add(:redis_pool_size, type: :integer)
       config.add(:redis_url, type: :string)
+      config.add(:requests_per_ip_limit, type: :integer)
+      config.add(:requests_per_ip_period, type: :integer)
       config.add(:saml_sp_issuer, type: :string)
       config.add(:saml_sp_private_key, type: :string)
       config.add(:saml_sp_private_key_password, type: :string)
