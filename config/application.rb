6d61fca7fe51970fd6cce52756247292b7368448
Howard Miller <howard.miller@gsa.gov>
2026-09-21 13:03:16 -0400

    Remove autoload of `lib/`
    We manually require the lib files we need in `config/application.rb`, and everything else in that folder would cause problems.
    
    changelog: Internal, Dependencies, Fix a config problem from upgrading to Rails 8

--- config/application.rb
+++ config/application.rb
@@ -31,12 +31,6 @@ module IdentityDashboard
     config.assets.unknown_asset_fallback = true
     config.action_view.button_to_generates_button_tag = false
 
-
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks])
-
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
